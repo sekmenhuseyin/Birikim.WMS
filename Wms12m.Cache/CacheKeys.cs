2c007d590e1c0722681cfaec34247e1fc73653de
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-02-16 10:09:47 +0300

    added cache and log

---
+++ Wms12m.Cache/CacheKeys.cs
@@ -0,0 +1,19 @@
+﻿namespace Wms12m.Cache
+{
+    public class CacheKeys
+    {
+        public class PersonsCacheKeys
+        {
+            public const string DataListOfUSER01 = "DataListOfUSER01";
+        }
+        public class StoreCacheKeys
+        {
+            public const string DataListOfStore = "DataListOfStore";
+            public const string DataListOfCorridor = "DataListOfCorridor";
+            public const string DataListOfShelf = "DataListOfShelf";
+            public const string DataListOfChapter = "DataListOfChapter";
+            public const string DataListOfFloor = "DataListOfFloor";
+            public const string DataListOfSize = "DataListOfSize";
+        }
+    }
+}
