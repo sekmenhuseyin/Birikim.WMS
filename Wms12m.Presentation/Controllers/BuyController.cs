cace8eab3b14dc26b90850ecebcbe38fb59cdcbd
yazilim4 <yazilim4@Y2>
2017-02-16 10:33:25 +0300

    s

--- Wms12m.Presentation/Controllers/BuyController.cs
+++ Wms12m.Presentation/Controllers/BuyController.cs
@@ -47,11 +47,9 @@ namespace Wms12m.Presentation.Controllers
             //add new
             STI tablo = new STI();
             tablo.IrsaliyeID = tbl.IrsaliyeId;
-            //tablo.Depo = tbl.Depo;
             tablo.MalKodu = tbl.MalKodu;
             tablo.Miktar = tbl.Miktar;
             tablo.Birim = tbl.Birim;
-            //tablo.MalzemeAdi = tbl.MalAdi;
             db.STIs.Add(tablo);
             db.SaveChanges();
             //get list
