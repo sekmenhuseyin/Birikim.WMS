ddc853c48ff319eac6bd5e1237f0bc9b0ae59569
yazilim4 <yazilim4@Y2>
2017-02-16 10:25:46 +0300

    gorev added

---
+++ Wms12m.Entity/Gorev/frmGorev.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace Wms12m.Entity
+{
+    public class frmGorev
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [DataMember, DisplayName("Depo"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public int DepoID { get; set; }
+
+        [DataMember, DisplayName("Görev No"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public string GorevNo { get; set; }
+
+        [DataMember, DisplayName("Görev Tipi"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public short GorevTipi { get; set; }
+
+        [DataMember, DisplayName("Görev Durumu"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public short Durum { get; set; }
+
+        [DataMember, DisplayName("Görevli"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public int GorevliID { get; set; }
+
+        [DataMember, DisplayName("Atayan"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public int AtayanID { get; set; }
+
+        [DataMember, DisplayName("Oluşturma Tarihi"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public int OlusturmaTarihi { get; set; }
+        [DataMember, DisplayName("Atama Tarihi"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public int AtamaTarihi { get; set; }
+        [DataMember, DisplayName("Bitirme Tarihi"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public int BitirmeTarihi { get; set; }
+        [DataMember, DisplayName("Bilgi"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public int Bilgi { get; set; }
+        [DataMember, DisplayName("Açıklama"), Required(ErrorMessage = "Boş bırakmayınız")]
+        public int Aciklama { get; set; }
+    }
+}
--- Wms12m.Presentation/Controllers/GorevController.cs
+++ Wms12m.Presentation/Controllers/GorevController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Wms12m.Entity;
 
 namespace Wms12m.Presentation.Controllers
 {
-    public class GorevController : Controller
+    public class GorevController : RootController
     {
         // GET: Gorev
         public ActionResult Index()
         {
-            return View();
+            return View("Index", new frmGorev());
+        }
+        public PartialViewResult GorevGridPartial()
+        {
+            var list = db.GorevListesis.ToList();
+            return PartialView("_GorevGridPartial", list);
         }
     }
 }
