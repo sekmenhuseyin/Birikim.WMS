6b4b0ea8ea5033790b0b59cd05f7f18d5e93cbfd
yazilim4 <yazilim4@Y2>
2017-02-22 11:51:08 +0300

    project update

---
+++ Wms12m.Business/Gorev/Gorev.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Linq;
+using Wms12m.Entity;
+using Wms12m.Entity.Models;
+using Wms12m.Security;
+using Wms12m.Configuration;
+
+namespace Wms12m.Business
+{
+    public class Gorev
+    {
+        Result _Result;
+        WMSEntities db = new WMSEntities();
+        CustomPrincipal Users = HttpContext.Current.User as CustomPrincipal;
+        /// <summary>
+        /// yeni ekle
+        /// </summary>
+        public Result Insert(frmIrsaliye tbl)
+        {
+            return _Result;
+        }
+    }
+}
