066464208b135743a35d81a6d9200953fc82d334
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-02-28 18:35:11 +0300

    boyut eklendi

---
+++ Wms12m.Presentation/Controllers/SizeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Controllers
+{
+    public class SizeController : Controller
+    {
+        /// <summary>
+        /// boyutlar anasayfası
+        /// </summary>
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
