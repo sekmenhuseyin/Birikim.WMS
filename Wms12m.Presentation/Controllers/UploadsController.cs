22ff1c37851ed9f5a60f464bcc17528dcab9dd71
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-02-28 17:26:16 +0300

    fa and uploads updated

--- Wms12m.Presentation/Controllers/UploadsController.cs
+++ Wms12m.Presentation/Controllers/UploadsController.cs
@@ -3,10 +3,11 @@ using System;
 using System.IO;
 using System.Web;
 using System.Data;
+using System.Linq;
+using Wms12m.Entity;
 using System.Web.Mvc;
+using Wms12m.Business;
 using Wms12m.Entity.Models;
-using System.Collections.Generic;
-
 
 namespace Wms12m.Presentation.Controllers
 {
@@ -15,49 +16,51 @@ namespace Wms12m.Presentation.Controllers
         /// <summary>
         /// irsaliye için malzeme girişi yapar
         /// </summary>
-        public JsonResult Irsaliye(int IrsNo,HttpPostedFileBase file)
+        public JsonResult Irsaliye(int IrsNo, HttpPostedFileBase file)
         {
-            if (file != null && file.ContentLength > 0)
+            if (file == null || file.ContentLength == 0) return null;
+            Stream stream = file.InputStream;
+            IExcelDataReader reader;
+            Result _Result = null;
+            //dosya tipini bul
+            if (file.FileName.EndsWith(".xls"))
+                reader = ExcelReaderFactory.CreateBinaryReader(stream);
+            else if (file.FileName.EndsWith(".xlsx"))
+                reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            else
+                return null;
+            //ilk satır başlık
+            reader.IsFirstRowAsColumnNames = true;
+            DataSet result = reader.AsDataSet();
+            if (result.Tables[0].Rows != null)
             {
-                List<WMS_STI> list_sti = new List<WMS_STI>();
-                Stream stream = file.InputStream;
-                IExcelDataReader reader = null;
-                //dosya tipini bul
-                if (file.FileName.EndsWith(".xls"))
-                    reader = ExcelReaderFactory.CreateBinaryReader(stream);
-                else if (file.FileName.EndsWith(".xlsx"))
-                    reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
-                else
-                    return null;
-                //ilk satır başlık
-                reader.IsFirstRowAsColumnNames = true;
-                DataSet result = reader.AsDataSet();
-                try
+                var irsaliye = db.WMS_IRS.Where(m => m.ID == IrsNo).FirstOrDefault();
+                using (DinamikModelContext Dinamik = new DinamikModelContext(irsaliye.SirketKod))
                 {
-                    if (result.Tables[0].Rows != null)
+                    for (int i = 0; i < result.Tables[0].Rows.Count; i++)
                     {
-                        for (int i = 0; i < result.Tables[0].Rows.Count; i++)
+                        DataRow dr = result.Tables[0].Rows[0];
+                        string malkodu = Dinamik.Context.TTies.Where(m => m.SatMalKodu == dr["MalKodu"].ToString() && m.Chk == irsaliye.HesapKodu).Select(m => m.MalKodu).FirstOrDefault();
+                        //add new
+                        try
                         {
-                            DataRow dr = result.Tables[0].Rows[0];
-                            var sti = new WMS_STI();
+                            WMS_STI sti = new WMS_STI();
                             sti.IrsaliyeID = IrsNo;
-                            sti.MalKodu = dr["MalKodu"].ToString();
+                            sti.MalKodu = malkodu;
                             sti.Miktar = Convert.ToDecimal(dr["Miktar"]);
                             sti.Birim = dr["Birim"].ToString();
-                            db.WMS_STI.Add(sti);
-                            db.SaveChanges();
-                            //list_sti.Add(sti);
+                            Stok tmpTable = new Stok();
+                            _Result = tmpTable.Operation(sti);
                         }
-                        //db.SaveChanges();
+                        catch (Exception)
+                        {
+                            return null;
+                        }
                     }
                 }
-                catch (Exception)
-                {
-                    return null;
-                }
-                reader.Close();
             }
-            return Json(true, JsonRequestBehavior.AllowGet);
+            reader.Close();
+            return Json(_Result, JsonRequestBehavior.AllowGet);
         }
     }
 }
