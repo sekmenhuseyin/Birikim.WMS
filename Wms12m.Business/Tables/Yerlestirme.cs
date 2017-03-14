5a08315818c5f8b7f6ef16373e2c4546e531755a
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-03-14 10:52:50 +0300

    yerleştirme biz added

---
+++ Wms12m.Business/Tables/Yerlestirme.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Wms12m.Entity;
+using Wms12m.Entity.Models;
+using Wms12m.Security;
+
+namespace Wms12m.Business
+{
+    public class Yerlestirme : abstractTables<Yer>, IDisposable
+    {
+        Result _Result;
+        WMSEntities db = new WMSEntities();
+        CustomPrincipal Users = HttpContext.Current.User as CustomPrincipal;
+        /// <summary>
+        /// burada yok
+        /// </summary>
+        public override Result Operation(Yer tbl)
+        {
+            return new Result();
+        }
+        /// <summary>
+        /// depo silme
+        /// </summary>
+        public override Result Delete(int Id)
+        {
+            _Result = new Result();
+            try
+            {
+                Yer tbl = db.Yers.Where(m => m.ID == Id).FirstOrDefault();
+                if (tbl != null)
+                {
+                    db.Yers.Remove(tbl);
+                    Yer_Log logs = new Yer_Log();
+                    logs.HucreAd = tbl.HucreAd;
+                    logs.MalKodu = tbl.MalKodu;
+                    logs.Birim = tbl.Birim;
+                    logs.Miktar = tbl.Miktar;
+                    logs.GC = true;
+                    logs.Kaydeden = Users.AppIdentity.User.Id;
+                    logs.KayitTarihi = DateTime.Today.ToOADateInt();
+                    logs.KayitSaati = DateTime.Now.SaatiAl();
+                    db.Yer_Log.Add(logs);
+                    db.SaveChanges();
+                    _Result.Id = Id;
+                    _Result.Message = "İşlem Başarılı !!!";
+                    _Result.Status = true;
+                }
+                else
+                {
+                    _Result.Message = "Kayıt Yok";
+                    _Result.Status = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _Result.Message = ex.Message + ": " + ex.InnerException.Message;
+                _Result.Status = false;
+            }
+            return _Result;
+        }
+        /// <summary>
+        /// depo bilgileri
+        /// </summary>
+        public override Yer Detail(int Id)
+        {
+            try
+            {
+                return db.Yers.Where(m => m.ID == Id).FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                return new Yer();
+            }
+        }
+        /// <summary>
+        /// depo listesi
+        /// </summary>
+        public override List<Yer> GetList()
+        {
+            return db.Yers.OrderBy(m=>m.MalKodu).ToList();
+        }
+        /// <summary>
+        /// üst tabloya ait olanları getir
+        /// </summary>
+        public override List<Yer> GetList(int ParentId)
+        {
+            return db.Yers.Where(m => m.KatID == ParentId).OrderBy(m => m.MalKodu).ToList();
+        }
+        /// <summary>
+        /// dispose
+        /// </summary>
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
