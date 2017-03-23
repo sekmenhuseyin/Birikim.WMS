779eef15025a56d4fd004d40dc6eebf79c59825f
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-03-23 18:20:37 +0300

    added stok

---
+++ Wms12m.Presentation/Controllers/StockController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Controllers
+{
+    public class StockController : RootController
+    {
+        /// <summary>
+        /// stok ana sayfası
+        /// </summary>
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
