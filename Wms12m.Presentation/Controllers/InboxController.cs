04540894ff32e3c73f3c617cefdbdc66710e2acc
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-04-25 12:13:29 +0300

    new pages added

---
+++ Wms12m.Presentation/Controllers/InboxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Controllers
+{
+    public class InboxController : Controller
+    {
+        // GET: Inbox
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
