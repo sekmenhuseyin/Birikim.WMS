3d2ad31a7643fb1606b8ca450f1c1cd3d1673671
Derviş Akdeniz <d.akdeniz@12mconsulting.com.tr>
2017-05-12 12:03:24 +0300

    aaa

--- Wms12m.Presentation/Controllers/FaturaOnayController.cs
+++ Wms12m.Presentation/Controllers/FaturaOnayController.cs
@@ -29,18 +29,21 @@ namespace Wms12m.Presentation.Controllers
         public ActionResult FaturaDetay(string EvrakNo)
         {
             FaturaDetayData _FDD = new FaturaDetayData();
+            _FDD.GENEL = new List<FaturaDetayGenel>();
+            _FDD.STI = new List<FaturaDetaySTI>();
+            _FDD.FTD = new List<FaturaDetayFTD>();
             var FDD = db.MultipleResults(string.Format("[FINSAT6{0}].[dbo].[FaturaOnayDetay] @EvrakNo='{1}'", 17, "VAS000000791")).With<FaturaDetayGenel>().With<FaturaDetaySTI>().With<FaturaDetayFTD>().Execute();
-            foreach (var item in FDD[0])
+            foreach (FaturaDetayGenel item in FDD[0])
             {
-                _FDD.GENEL.Add((FaturaDetayGenel)item);
+                _FDD.GENEL.Add(item);
             }
-            foreach (var item in FDD[1])
+            foreach (FaturaDetaySTI item in FDD[1])
             {
-                _FDD.STI.Add((FaturaDetaySTI)item);
+                _FDD.STI.Add(item);
             }
-            foreach (var item in FDD[2])
+            foreach (FaturaDetayFTD item in FDD[2])
             {
-                _FDD.FTD.Add((FaturaDetayFTD)item);
+                _FDD.FTD.Add(item);
             }
             ViewBag.EvrakNo = EvrakNo;
             return View(_FDD);
