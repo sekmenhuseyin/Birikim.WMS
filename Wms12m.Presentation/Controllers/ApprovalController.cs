bd25e1fdd9846dd158e093b844ae4d4de06b0b9b
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-05-16 13:01:57 +0300

    fiyat onay birleşti

--- Wms12m.Presentation/Controllers/ApprovalController.cs
+++ Wms12m.Presentation/Controllers/ApprovalController.cs
@@ -6,6 +6,7 @@ namespace Wms12m.Presentation.Controllers
 {
     public class ApprovalController : RootController
     {
+        #region Çek
         public ActionResult Cek_SPGMY()
         {
             if (CheckPerm("Çek Onaylama", PermTypes.Reading) == false) return Redirect("/");
@@ -41,5 +42,35 @@ namespace Wms12m.Presentation.Controllers
             var KOD = db.Database.SqlQuery<CekOnaySelect>(string.Format("[FINSAT6{0}].[wms].[CekOnayGM]", "33")).ToList();
             return PartialView(KOD);
         }
+        #endregion
+        #region Fiyat
+        public ActionResult Fiyat_GM()
+        {
+            return View();
+        }
+        public PartialViewResult Fiyat_GM_List()
+        {
+            var KOD = db.Database.SqlQuery<FiyatOnayGMSelect>(string.Format("[FINSAT6{0}].[wms].[FiyatOnayListGM]", "33")).ToList();
+            return PartialView(KOD);
+        }
+        public ActionResult Fiyat_SPGMY()
+        {
+            return View();
+        }
+        public PartialViewResult Fiyat_SPGMY_List()
+        {
+            var KOD = db.Database.SqlQuery<FiyatOnayGMSelect>(string.Format("[FINSAT6{0}].[wms].[FiyatOnayListSPGMY]", "33")).ToList();
+            return PartialView(KOD);
+        }
+        public ActionResult Fiyat_SM()
+        {
+            return View();
+        }
+        public PartialViewResult Fiyat_SM_List()
+        {
+            var KOD = db.Database.SqlQuery<FiyatOnayGMSelect>(string.Format("[FINSAT6{0}].[wms].[FiyatOnayList]", "33")).ToList();
+            return PartialView(KOD);
+        }
+        #endregion
     }
 }
