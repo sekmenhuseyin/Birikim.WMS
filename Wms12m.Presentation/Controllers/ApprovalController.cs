5c92b78ff4fd43a34888a1b931db45325940c9fb
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-05-16 11:23:46 +0300

    çok onay birleşti

--- Wms12m.Presentation/Controllers/ApprovalController.cs
+++ Wms12m.Presentation/Controllers/ApprovalController.cs
@@ -1,17 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using Wms12m.Entity;
 
 namespace Wms12m.Presentation.Controllers
 {
     public class ApprovalController : RootController
     {
-        // GET: Approval
-        public ActionResult Index()
+        public ActionResult Cek_SPGMY()
         {
             return View();
+        }
+        public PartialViewResult Cek_SPGMY_List()
+        {
+            var KOD = db.Database.SqlQuery<CekOnaySelect>(string.Format("[FINSAT6{0}].[wms].[CekOnaySPGMY]", "33")).ToList();
+            return PartialView(KOD);
+        }
+
+        public ActionResult Cek_MIGMY()
+        {
+
+            return View();
+        }
+        public PartialViewResult Cek_MIGMY_List()
+        {
+            var KOD = db.Database.SqlQuery<CekOnaySelect>(string.Format("[FINSAT6{0}].[wms].[CekOnayMIGMY]", "33")).ToList();
+            return PartialView(KOD);
+        }
+
+        public ActionResult Cek_GM()
+        {
+
+            return View();
+        }
+        public PartialViewResult Cek_GM_List()
+        {
+            var KOD = db.Database.SqlQuery<CekOnaySelect>(string.Format("[FINSAT6{0}].[wms].[CekOnayGM]", "33")).ToList();
+            return PartialView(KOD);
         }
     }
 }
