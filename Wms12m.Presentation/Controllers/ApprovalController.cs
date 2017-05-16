2ae26336b692613646bc37ed5cb68afdc12309d5
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-05-16 11:29:04 +0300

    cok onay update

--- Wms12m.Presentation/Controllers/ApprovalController.cs
+++ Wms12m.Presentation/Controllers/ApprovalController.cs
@@ -8,32 +8,36 @@ namespace Wms12m.Presentation.Controllers
     {
         public ActionResult Cek_SPGMY()
         {
+            if (CheckPerm("Çek Onaylama", PermTypes.Reading) == false) return Redirect("/");
             return View();
         }
         public PartialViewResult Cek_SPGMY_List()
         {
+            if (CheckPerm("Çek Onaylama", PermTypes.Reading) == false) return null;
             var KOD = db.Database.SqlQuery<CekOnaySelect>(string.Format("[FINSAT6{0}].[wms].[CekOnaySPGMY]", "33")).ToList();
             return PartialView(KOD);
         }
 
         public ActionResult Cek_MIGMY()
         {
-
+            if (CheckPerm("Çek Onaylama", PermTypes.Reading) == false) return Redirect("/");
             return View();
         }
         public PartialViewResult Cek_MIGMY_List()
         {
+            if (CheckPerm("Çek Onaylama", PermTypes.Reading) == false) return null;
             var KOD = db.Database.SqlQuery<CekOnaySelect>(string.Format("[FINSAT6{0}].[wms].[CekOnayMIGMY]", "33")).ToList();
             return PartialView(KOD);
         }
 
         public ActionResult Cek_GM()
         {
-
+            if (CheckPerm("Çek Onaylama", PermTypes.Reading) == false) return Redirect("/");
             return View();
         }
         public PartialViewResult Cek_GM_List()
         {
+            if (CheckPerm("Çek Onaylama", PermTypes.Reading) == false) return null;
             var KOD = db.Database.SqlQuery<CekOnaySelect>(string.Format("[FINSAT6{0}].[wms].[CekOnayGM]", "33")).ToList();
             return PartialView(KOD);
         }
