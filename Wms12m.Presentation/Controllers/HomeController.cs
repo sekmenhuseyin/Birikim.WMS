1835fcb25ca37638b5c4dc5bb7b3a67e57f73dcc
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-05-29 10:05:49 +0300

    update

--- Wms12m.Presentation/Controllers/HomeController.cs
+++ Wms12m.Presentation/Controllers/HomeController.cs
@@ -192,10 +192,12 @@ namespace Wms12m.Presentation.Controllers
             return PartialView("_PartialLokasyonSatisKriter", UGSK);
         }
 
-        public PartialViewResult PartialBakiyeRiskAnalizi()
+        public PartialViewResult PartialBakiyeRiskAnalizi(string SirketKodu)
         {
             if (CheckPerm("ChartBakiyeRiskAnalizi", PermTypes.Reading) == false) return null;
             var BRA = db.Database.SqlQuery<ChartBakiyeRiskAnalizi>(string.Format("[FINSAT6{0}].[wms].[DB_BakiyeRiskAnalizi]", "33")).ToList();
+            ViewBag.SirketKodu = SirketKodu;
+            ViewBag.SirketID = new SelectList(db.GetSirkets().ToList(), "Kod", "Ad");
             return PartialView("_PartialBakiyeRiskAnalizi", BRA);
         }
 
