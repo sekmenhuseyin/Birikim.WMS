4f260dff95a7c34be92c314e14561386d492e1d4
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-05-29 16:04:32 +0300

    barkod yine

--- Wms12m.Presentation/Controllers/TerminalController.cs
+++ Wms12m.Presentation/Controllers/TerminalController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Wms12m.Entity;
 using Wms12m.Entity.Models;
@@ -83,34 +79,6 @@ namespace Wms12m.Presentation.Controllers
             if (CheckPerm("Terminal için Yetkilendirme", PermTypes.Deleting) == false) return Json(new Result(false, "Yetkiniz yok"), JsonRequestBehavior.AllowGet);
             Result _Result = PersonPerms.Delete(Id);
             return Json(_Result, JsonRequestBehavior.AllowGet);
-        }
-    }
-}
-namespace Wms12m
-{
-    public class IDAtomationBarCode
-    {
-        public static string BarcodeImageGenerator(string Code)
-        {
-            byte[] BarCode;
-            string BarCodeImage;
-            Bitmap objBitmap = new Bitmap(Code.Length * 28, 100);
-            using (Graphics graphic = Graphics.FromImage(objBitmap))
-            {
-                Font newFont = new Font("IDAutomationHC39M Free Version", 18, FontStyle.Regular);
-                PointF point = new PointF(2, 2);
-                SolidBrush balck = new SolidBrush(Color.Black);
-                SolidBrush white = new SolidBrush(Color.White);
-                graphic.FillRectangle(white, 0, 0, objBitmap.Width, objBitmap.Height);
-                graphic.DrawString("*" + Code + "*", newFont, balck, point);
-            }
-            using (MemoryStream Mmst = new MemoryStream())
-            {
-                objBitmap.Save(Mmst, ImageFormat.Png);
-                BarCode = Mmst.GetBuffer();
-                BarCodeImage = BarCode != null ? "data:image/jpg;base64," + Convert.ToBase64String((byte[])BarCode) : "";
-                return BarCodeImage;
-            }
         }
     }
 }
