2dbc5b53e0110f399d885e3969d0e87dbdb66755
Ruhi Göktaş <rgoktash@outlook.com>
2017-06-01 13:23:55 +0300

    musteri

---
+++ Wms12m.Presentation/Controllers/CustomerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Wms12m.Entity.Models;
+
+namespace Wms12m.Presentation.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private WMSEntities db = new WMSEntities();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(db.Musteris.ToList());
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musteri musteri = db.Musteris.Find(id);
+            if (musteri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musteri);
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Firma,Unvan,Aciklama,Email,Tel1,Tel2,MesaiKontrol,MesaiKota,Kaydeden,KayitTarih,Degistiren,DegisTarih")] Musteri musteri)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Musteris.Add(musteri);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(musteri);
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musteri musteri = db.Musteris.Find(id);
+            if (musteri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musteri);
+        }
+
+        // POST: Customer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Firma,Unvan,Aciklama,Email,Tel1,Tel2,MesaiKontrol,MesaiKota,Kaydeden,KayitTarih,Degistiren,DegisTarih")] Musteri musteri)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(musteri).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(musteri);
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Musteri musteri = db.Musteris.Find(id);
+            if (musteri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(musteri);
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Musteri musteri = db.Musteris.Find(id);
+            db.Musteris.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
