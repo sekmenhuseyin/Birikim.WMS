2067fb8f61a65d7828de10053bc436fa1a435fb5
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-06-02 13:04:10 +0300

    files updated

---
+++ Wms12m.Presentation/Areas/Approval/Controllers/ContractController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Areas.Approval.Controllers
+{
+    public class ContractController : RootController
+    {
+        // GET: Approval/Contract
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
