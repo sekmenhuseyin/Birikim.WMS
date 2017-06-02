98f0b4b2ba5d685d6bdd022c3a94af33f7b87faf
Ruhi Göktaş <rgoktash@outlook.com>
2017-06-02 15:06:44 +0300

    DutiesController

---
+++ Wms12m.Presentation/Controllers/DutiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Wms12m.Entity.Models;
+
+namespace Wms12m.Presentation.Controllers
+{
+    public class DutiesController : RootController
+    {
+        private WMSEntities db = new WMSEntities();
+
+        // GET: Duties
+        public ActionResult Index()
+        {
+            var gorevlers = db.Gorevlers.Include(g => g.ProjeForm);
+            return View(gorevlers.ToList());
+        }
+
+        // GET: Duties/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gorevler gorevler = db.Gorevlers.Find(id);
+            if (gorevler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gorevler);
+        }
+
+        // GET: Duties/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProjeFormID = new SelectList(db.ProjeForms, "ID", "Proje");
+            return View();
+        }
+
+        // POST: Duties/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,ProjeFormID,Sorumlu,Sorumlu2,Sorumlu3,Gorev,Aciklama,Oncelik,Durum,GorevTipi,Departman,TahminiBitis,BitisTarih,IslemTip,IslemSira,Kaydeden,KayitTarih,Degistiren,DegisTarih")] Gorevler gorevler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Gorevlers.Add(gorevler);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProjeFormID = new SelectList(db.ProjeForms, "ID", "Proje", gorevler.ProjeFormID);
+            return View(gorevler);
+        }
+
+        // GET: Duties/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gorevler gorevler = db.Gorevlers.Find(id);
+            if (gorevler == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjeFormID = new SelectList(db.ProjeForms, "ID", "Proje", gorevler.ProjeFormID);
+            return View(gorevler);
+        }
+
+        // POST: Duties/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,ProjeFormID,Sorumlu,Sorumlu2,Sorumlu3,Gorev,Aciklama,Oncelik,Durum,GorevTipi,Departman,TahminiBitis,BitisTarih,IslemTip,IslemSira,Kaydeden,KayitTarih,Degistiren,DegisTarih")] Gorevler gorevler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gorevler).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjeFormID = new SelectList(db.ProjeForms, "ID", "Proje", gorevler.ProjeFormID);
+            return View(gorevler);
+        }
+
+        // GET: Duties/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gorevler gorevler = db.Gorevlers.Find(id);
+            if (gorevler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gorevler);
+        }
+
+        // POST: Duties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Gorevler gorevler = db.Gorevlers.Find(id);
+            db.Gorevlers.Remove(gorevler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
