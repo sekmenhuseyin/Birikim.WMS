5f672f5dc2320a0baddb5c09afa465eb33f6bfa4
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-06-02 10:37:23 +0300

    area added

---
+++ Wms12m.Presentation/Areas/Approval/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Areas.Approval.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Approval/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
