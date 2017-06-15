0a8502954861774ee3aebeec95d2f378bdc895ca
Derviş Akdeniz <d.akdeniz@12mconsulting.com.tr>
2017-06-15 12:01:18 +0300

    aa

--- Wms12m.Presentation/Areas/Approvals/Controllers/TechnoController.cs
+++ Wms12m.Presentation/Areas/Approvals/Controllers/TechnoController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
@@ -25,6 +28,13 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
         {
             if (CheckPerm("Fiyat Onaylama", PermTypes.Reading) == false) return null;
             return PartialView();
+        }
+
+        public PartialViewResult EskiUcretData(string PERSONELID)
+        {
+            if (CheckPerm("Sözleşme Onaylama", PermTypes.Reading) == false) return null;
+            var list = db.Database.SqlQuery<TechnoList>(string.Format("[HR0312M].[dbo].[TCH_EskiUcretSelect] {0}", PERSONELID)).ToList();
+            return PartialView(list);
         }
 
         public string UcretListData()
@@ -62,11 +72,34 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             return json.Serialize(primBilgi);
         }
 
-        public PartialViewResult EskiUcretData(string PERSONELID)
+
+
+        public JsonResult Ucret_Onayla(string Data)
         {
-            if (CheckPerm("Sözleşme Onaylama", PermTypes.Reading) == false) return null;
-            var list = db.Database.SqlQuery<TechnoList>(string.Format("[HR0312M].[dbo].[TCH_EskiUcretSelect] {0}", PERSONELID)).ToList();
-            return PartialView(list);
+            Result _Result = new Result(true);
+            if (CheckPerm("Sözleşme Onaylama", PermTypes.Writing) == false) return null;
+            JArray parameters = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JArray>(Request["Data"]);
+            SqlExper sqlexper = new SqlExper(ConfigurationManager.ConnectionStrings["WMSConnection"].ConnectionString, "17");
+            try
+            {
+                foreach (JObject insertObj in parameters)
+                {
+                    var IslemTip = insertObj["IslemTip"];
+                    var IslemTarih = insertObj["IslemTarih"];
+                    var ButUcretID = insertObj["ButUcretID"];
+                    var PersonelID = insertObj["PERSONELID"];
+                }
+                _Result.Status = true;
+                _Result.Message = "İşlem Başarılı ";
+
+            }
+            catch (Exception ex)
+            {
+                _Result.Status = false;
+                _Result.Message = "Hata Oluştu. ";
+
+            }
+            return Json(_Result, JsonRequestBehavior.AllowGet);
         }
     }
 }
