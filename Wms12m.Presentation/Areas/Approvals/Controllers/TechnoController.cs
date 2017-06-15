b3bb7822fc3d312d428e6981a5e572b15475eefc
Derviş Akdeniz <d.akdeniz@12mconsulting.com.tr>
2017-06-15 18:48:26 +0300

    aa

--- Wms12m.Presentation/Areas/Approvals/Controllers/TechnoController.cs
+++ Wms12m.Presentation/Areas/Approvals/Controllers/TechnoController.cs
@@ -18,15 +18,17 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             return View();
         }
 
-        public PartialViewResult Ucret_List()
+        public PartialViewResult Ucret_List(string Tip)
         {
             if (CheckPerm("Fiyat Onaylama", PermTypes.Reading) == false) return null;
+            ViewBag.Tip = Tip;
             return PartialView();
         }
 
-        public PartialViewResult Prim_List()
+        public PartialViewResult Prim_List(string Tip)
         {
             if (CheckPerm("Fiyat Onaylama", PermTypes.Reading) == false) return null;
+            ViewBag.Tip = Tip;
             return PartialView();
         }
 
@@ -37,7 +39,7 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             return PartialView(list);
         }
 
-        public string UcretListData()
+        public string UcretListData(string tip)
         {
             JavaScriptSerializer json = new JavaScriptSerializer()
             {
@@ -46,7 +48,7 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             List<TechnoList> ucretBilgi;
             try
             {
-                ucretBilgi = db.Database.SqlQuery<TechnoList>(string.Format("[HR0312M].[dbo].[TCH_UcretOnaySelect] @Birim='GM'")).ToList();
+                ucretBilgi = db.Database.SqlQuery<TechnoList>(string.Format("[HR0312M].[dbo].[TCH_UcretOnaySelect] @Birim='GM', @Tip='{0}'", tip)).ToList();
             }
             catch (Exception ex)
             {
@@ -54,7 +56,7 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             }
             return json.Serialize(ucretBilgi);
         }
-        public string PrimListData()
+        public string PrimListData(string tip)
         {
             JavaScriptSerializer json = new JavaScriptSerializer()
             {
@@ -63,7 +65,7 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             List<TechnoList> primBilgi;
             try
             {
-                primBilgi = db.Database.SqlQuery<TechnoList>(string.Format("[HR0312M].[dbo].[TCH_PrimOnaySelect]")).ToList();
+                primBilgi = db.Database.SqlQuery<TechnoList>(string.Format("[HR0312M].[dbo].[TCH_PrimOnaySelect] @Birim='IK', @Tip='{0}'", tip)).ToList();
             }
             catch (Exception ex)
             {
@@ -86,8 +88,39 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
                 {
                     var ID = insertObj["ID"];
                     var OnayDerece = 1;
-                    string s = string.Format("[HR0312M].[dbo].[TCH_UcretOnayUpdate] @OnayDerece={0}, @ID={1}", OnayDerece, ID);
+                    string s = string.Format("[HR0312M].[dbo].[TCH_UcretOnayUpdate] @OnayDerece={0}, @ID={1},@Birim='GM'", OnayDerece, ID);
                     var x = db.Database.SqlQuery<int>(s).ToList();
+                    string ss = string.Format("[HR0312M].[dbo].[TCH_BUTUCRETINSERT] @ID={0}", ID);
+                    var xx = db.Database.SqlQuery<int>(ss).ToList();
+                }
+                _Result.Status = true;
+                _Result.Message = "İşlem Başarılı ";
+
+            }
+            catch (Exception ex)
+            {
+                _Result.Status = false;
+                _Result.Message = "Hata Oluştu. ";
+
+            }
+            return Json(_Result, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Prim_Onayla(string Data)
+        {
+            Result _Result = new Result(true);
+            if (CheckPerm("Sözleşme Onaylama", PermTypes.Writing) == false) return null;
+            JArray parameters = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JArray>(Request["Data"]);
+            SqlExper sqlexper = new SqlExper(ConfigurationManager.ConnectionStrings["WMSConnection"].ConnectionString, "17");
+            try
+            {
+                foreach (JObject insertObj in parameters)
+                {
+                    var ID = insertObj["ID"];
+                    var OnayDerece = 1;
+                    string s = string.Format("[HR0312M].[dbo].[TCH_PrimOnayUpdate] @OnayDerece={0}, @ID={1},@Birim='IK'", OnayDerece, ID);
+                    var x = db.Database.SqlQuery<int>(s).ToList();
+                    string ss = string.Format("[HR0312M].[dbo].[TCH_BRDSKALAINSERT] @ID={0}", ID);
+                    var xx = db.Database.SqlQuery<int>(ss).ToList();
                 }
                 _Result.Status = true;
                 _Result.Message = "İşlem Başarılı ";
