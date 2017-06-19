bc88221f5123132d2e33c96daf4061ae8351003f
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-06-19 17:58:47 +0300

    update

---
+++ Wms12m.Presentation/Controllers/HelpController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Controllers
+{
+    public class HelpController : RootController
+    {
+        // GET: Help
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Wms12m.Presentation/Controllers/LogsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Controllers
+{
+    public class LogsController : RootController
+    {
+        // GET: Logs
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Error()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult App()
+        {
+            return View();
+        }
+    }
+}
