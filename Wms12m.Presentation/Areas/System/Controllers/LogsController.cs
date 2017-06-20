a400bfd3c4a867ae27594d654ade97a5f95f12b8
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-06-20 15:50:30 +0300

    update

--- Wms12m.Presentation/Areas/System/Controllers/LogsController.cs
+++ Wms12m.Presentation/Areas/System/Controllers/LogsController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace Wms12m.Presentation.Controllers
+namespace Wms12m.Presentation.Areas.System.Controllers
 {
     public class LogsController : RootController
     {
---
+++ Wms12m.Presentation/Areas/System/Controllers/SettingsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Areas.System.Controllers
+{
+    public class SettingsController : RootController
+    {
+        // GET: System/Settings
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
