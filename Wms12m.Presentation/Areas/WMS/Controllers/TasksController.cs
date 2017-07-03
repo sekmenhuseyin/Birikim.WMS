3ba432f71f412216d9580f52e7e48d7dd8cce49a
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-07-03 10:54:38 +0300

    t

--- Wms12m.Presentation/Areas/WMS/Controllers/TasksController.cs
+++ Wms12m.Presentation/Areas/WMS/Controllers/TasksController.cs
@@ -472,7 +472,7 @@ namespace Wms12m.Presentation.Areas.WMS.Controllers
             tblx.DegisTarih = fn.ToOADate();
             tbl.Degistiren = vUser.UserName;
             db.SaveChanges();
-            ViewBag.Details = db.Database.SqlQuery<frmPaketBarkod>(string.Format("EXEC [FINSAT6{0}].[wms].[getBarcodeDetails] @SirketKodu = N'{0}', @DepoKodu = N'{1}', @EvrakNo = N'{2}'", tblx.SirketKodu, tblx.Gorev.Depo.DepoKodu, tblx.Gorev.IR.LinkEvrakNo)).FirstOrDefault();
+            ViewBag.Details = db.Database.SqlQuery<frmPaketBarkod>(string.Format("EXEC [FINSAT6{0}].[wms].[getBarcodeDetails] @SirketKodu = N'{1}', @DepoKodu = N'{2}', @EvrakNo = N'{3}'", tblx.Gorev.IR.SirketKod, tblx.SirketKodu, tblx.Gorev.Depo.DepoKodu, tblx.Gorev.IR.LinkEvrakNo)).FirstOrDefault();
             return View("BarcodePrint", tblx);
         }
     }
