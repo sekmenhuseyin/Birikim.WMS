82161d9d7c81d970da5911f5e7dc71b3772e9c57
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-07-13 17:12:25 +0300

    updaters

--- Wms12m.Entity/Viewmodels/frmUsers.cs
+++ Wms12m.Entity/Viewmodels/frmUsers.cs
@@ -43,4 +43,15 @@
         public string Updating { get; set; }
         public string Deleting { get; set; }
     }
+    /// <summary>
+    /// b2b kullanıcıları
+    /// </summary>
+    public class mdlB2BUsers
+    {
+        public int ID { get; set; }
+        public string HesapKodu { get; set; }
+        public string Unvan { get; set; }
+        public string YetkiliEMail { get; set; }
+        public string Parola { get; set; }
+    }
 }
--- Wms12m.Security/Properties/AssemblyInfo.cs
+++ Wms12m.Security/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Wms12m.Security")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("12M Consulting")]
 [assembly: AssemblyProduct("Wms12m.Security")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("tr-TR")]
+
