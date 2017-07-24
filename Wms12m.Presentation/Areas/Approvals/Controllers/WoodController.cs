ac362f10b7fc6c470f6e449092fab790c2aff4a2
Ruhi Göktaş <rgoktash@outlook.com>
2017-07-24 19:44:15 +0300

    wood controller

--- Wms12m.Presentation/Areas/Approvals/Controllers/WoodController.cs
+++ Wms12m.Presentation/Areas/Approvals/Controllers/WoodController.cs
@@ -37,7 +37,7 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             if (CheckPerm(Perms.TeminatOnay, PermTypes.Writing) == false) return null;
             JArray parameters = JsonConvert.DeserializeObject<JArray>(Request["Data"]);
             SqlExper sqlexper = new SqlExper(ConfigurationManager.ConnectionStrings["WMSConnection"].ConnectionString, "17");
-
+           
             try
             {
                 Dictionary<string, int> FiyatMaxSiraNo = new Dictionary<string, int>();
@@ -45,9 +45,32 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
                 {
                     DateTime date = DateTime.Now;
                     var shortDate = date.ToString("yyyy-MM-dd");
-                    var sonuc = sqlexper.AcceptChanges();
-                    db.Database.ExecuteSqlCommand(string.Format("[FINSAT6{0}].[wms].[TeminatOnayUpdate] @ID = {1}, @Kullanici = '{2}'", "17", insertObj["ID"].ToString(), vUser.UserName));
+                    //var sql = string.Format("SELECT * FROM [FINSAT6{0}].[FINSAT6{0}].[IHLTAH]  WHERE ID={1} AND ONAY = 0", "17", insertObj["ID"].ToString());
 
+                    //var sonuc = sqlexper.AcceptChanges();
+                    // IHLTAH ihltah = VKContext.IHLTAHs.Where(t => t.ID == item.ID && t.Onay == false).FirstOrDefault();
+                    //var ihltah1 = db.Database.SqlQuery<IHLTAH>(sql).ToList();
+                    string sql = "";
+                    if (insertObj["Tip"].ToShort() == (short)0)
+                    {
+                        sql = ",TavanMiktar=" + insertObj["TavanMiktar"].ToDecimal() + ", TavanFiyat=" + insertObj["TavanFiyat"].ToDecimal();
+                        //ihltah1[0].TavanMiktar = item.TavanMiktar;
+                        //ihltah1[0].TavanFiyat = item.TavanFiyat;
+
+                    }
+
+
+                    //foreach (IHLTAH item in ihltah1)
+                    //{
+                    //    if (item.Tip == 0) 
+                    //    {
+                    //        ihltah1[0].TavanMiktar = item.TavanMiktar;
+                    //        ihltah1[0].TavanFiyat = item.TavanFiyat;
+
+                    //    }
+                    //}
+
+                    db.Database.ExecuteSqlCommand(string.Format("UPDATE [FINSAT6{0}].[FINSAT6{0}].[IHLTAH] SET Onay = 1, Onaylayan='" + vUser.UserName + "', OnayTarih='{2}',DegisTarih='{2}''{3}'  where ID = '{1}' AND Onay=0", "17", insertObj["ID"].ToString(), shortDate, sql));
                 }
                 _Result.Status = true;
                 _Result.Message = "İşlem Başarılı ";
