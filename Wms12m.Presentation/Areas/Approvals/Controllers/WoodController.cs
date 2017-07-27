44eb01a46830a90ebf7f5b354d9ee2db7a7c54ce
Ruhi Göktaş <rgoktash@outlook.com>
2017-07-27 14:27:37 +0300

    TahsisliIsletmeKasasi

--- Wms12m.Presentation/Areas/Approvals/Controllers/WoodController.cs
+++ Wms12m.Presentation/Areas/Approvals/Controllers/WoodController.cs
@@ -119,7 +119,6 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
 
         }
 
-
         public ActionResult TahsisliAlim()
         {
 
@@ -170,20 +169,35 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             // if (CheckPerm(Perms.FiyatOnaylamaGM, PermTypes.Reading) == false) return Redirect("/");
             return View();
         }
-        public PartialViewResult TahsisliIsletmeKasasi_List()
+        public PartialViewResult TahsisliIsletmeKasasi_List(string EvrakNo,string HesapKodu)
         {
-            // if (CheckPerm(Perms.FiyatOnaylamaGM, PermTypes.Reading) == false) return null;
-            return PartialView();
-        }
+            var RT = db.Database.SqlQuery<MyChi>(string.Format(@"SELECT DISTINCT CHI.HesapKodu, CHK.Unvan1 as Unvan, CHI.EvrakNo, CHI.EvrakNo2
+                                                                , CAST(CHI.Tarih-2 as smalldatetime) as Tarih, CHI.Kod13, CHI.Kod14 
 
-        public PartialViewResult TahsisliIsletmeKasasi_Grid()
-        {
-            // if (CheckPerm(Perms.FiyatOnaylamaGM, PermTypes.Reading) == false) return null;
-            return PartialView();
+                                                                FROM FINSAT6{0}.FINSAT6{0}.CHI (nolock)
+                                                                LEFT JOIN FINSAT6{0}.FINSAT6{0}.CHK (nolock) ON CHK.HesapKodu=CHI.HesapKodu
+                                                                WHERE CHI.KynkEvrakTip=4 AND CHI.Kod13>0 AND CHI.Kod14>0 
+                                                                AND CHI.EvrakNo2='{1}' AND CHI.HesapKodu='{2}' ", "17", EvrakNo, HesapKodu)).ToList();
+
+            var RT1 = db.Database.SqlQuery<MySti>(string.Format(@"SELECT STI.EvrakNo, STI.Tarih, STI.Chk, STI.MalKodu, STK.MalAdi, STI.BirimMiktar, STI.Birim 
+                                                                  FROM FINSAT6{0}.FINSAT6{0}.STI (nolock)
+                                                                INNER JOIN FINSAT6{0}.FINSAT6{0}.STK (nolock) ON STK.MalKodu=STI.MalKodu
+                                                                WHERE STI.KynkEvrakTip=4 AND STI.Chk='{2}' AND STI.EvrakNo IN  
+                                                                (
+                                                                    SELECT EvrakNo FROM FINSAT6{0}.FINSAT6{0}.CHI (nolock) 
+                                                                    WHERE KynkEvrakTip=4 AND Kod13>0 AND Kod14>0 AND EvrakNo2='{1}' 
+                                                                    AND HesapKodu='{2}' AND BirimMiktar>0
+                                                                 )", "17", EvrakNo, HesapKodu)).ToList();
+
+
+            foreach (MyChi chi in RT)
+            {
+                chi.FaturaDetay = RT1.FindAll(t => t.EvrakNo == chi.EvrakNo);
+            }
+            return PartialView(RT);
         }
         public string KasaEvrakCek()
         {
-            //if (CheckPerm(Perms.FiyatOnaylamaGM, PermTypes.Reading) == false) return null;
             var RT = db.Database.SqlQuery<TahsisliIsletmeKasa>(string.Format(@"SELECT IHL.EvrakNo, IHL.OrmIslt, CHK.Unvan1 as OrmIsltUnvan, IHL.Yil, IHL.Hafta 
                                                                         , IHL.TahTopMektupTutar, IHL.TahPesinat
                                                                         , IHL.IbreliMiktarSter, IHL.IbreliMiktarM3
@@ -196,11 +210,8 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             var json = new JavaScriptSerializer().Serialize(RT);
             return json;
         }
-   
-
         public string KasaEvrakCekEvrakNoIle(string EvrakNo,string HesapKodu)
         {
-            //if (CheckPerm(Perms.FiyatOnaylamaGM, PermTypes.Reading) == false) return null;
             var RT = db.Database.SqlQuery<MyChi>(string.Format(@"SELECT DISTINCT CHI.HesapKodu, CHK.Unvan1 as Unvan, CHI.EvrakNo, CHI.EvrakNo2
                                                                 , CAST(CHI.Tarih-2 as smalldatetime) as Tarih, CHI.Kod13, CHI.Kod14 
 
@@ -230,5 +241,112 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             var json = new JavaScriptSerializer().Serialize(RT);
             return json;
         }
+
+
+        public ActionResult IhaleliOnay()
+        {
+            // if (CheckPerm(Perms.FiyatOnaylamaGM, PermTypes.Reading) == false) return Redirect("/");
+            return View();
+        }
+        public PartialViewResult IhaleliAlim_List()
+        {
+            // if (CheckPerm(Perms.FiyatOnaylamaGM, PermTypes.Reading) == false) return null;
+            return PartialView();
+        }
+        public string IhaleliAlimOnayCek()
+        {
+            if (CheckPerm(Perms.FiyatOnaylamaGM, PermTypes.Reading) == false) return null;
+            var RT = db.Database.SqlQuery<TahsisOnayOdun>(string.Format("[FINSAT6{0}].[dbo].[IHLTAHOnaydaBekleyen] @Tip = 0", "17")).ToList();
+            var json = new JavaScriptSerializer().Serialize(RT);
+            return json;
+        }
+        public JsonResult IhaleliOnay(string Data)
+        {
+            Result _Result = new Result(true);
+            //  if (CheckPerm(Perms.TeminatOnay, PermTypes.Writing) == false) return null;
+            JArray parameters = JsonConvert.DeserializeObject<JArray>(Request["Data"]);
+            SqlExper sqlexper = new SqlExper(ConfigurationManager.ConnectionStrings["WMSConnection"].ConnectionString, "17");
+
+            try
+            {
+
+                foreach (JObject insertObj in parameters)
+                {
+                    DateTime date = DateTime.Now;
+                    var shortDate = date.ToString("yyyy-MM-dd");
+                    //var sql = string.Format("SELECT * FROM [FINSAT6{0}].[FINSAT6{0}].[IHLTAH]  WHERE ID={1} AND ONAY = 0", "17", insertObj["ID"].ToString());
+
+                    //var sonuc = sqlexper.AcceptChanges();
+                    // IHLTAH ihltah = VKContext.IHLTAHs.Where(t => t.ID == item.ID && t.Onay == false).FirstOrDefault();
+                    //var ihltah1 = db.Database.SqlQuery<IHLTAH>(sql).ToList();
+                    string sql = "";
+                    if (insertObj["Tip"].ToShort() == (short)0)
+                    {
+                        if (insertObj["TavanMiktar"].ToDecimal() > 0)
+                            sql = ",TavanMiktar=" + insertObj["TavanMiktar"].ToString().Replace(',', '.').ToDecimal();
+                        if (insertObj["TavanMiktar"].ToDecimal() > 0)
+                            sql += ", TavanFiyat=" + insertObj["TavanFiyat"].ToString().Replace(',', '.').ToDecimal();
+                        //ihltah1[0].TavanMiktar = item.TavanMiktar;
+                        //ihltah1[0].TavanFiyat = item.TavanFiyat;
+
+                    }
+
+
+                    //foreach (IHLTAH item in ihltah1)
+                    //{
+                    //    if (item.Tip == 0) 
+                    //    {
+                    //        ihltah1[0].TavanMiktar = item.TavanMiktar;
+                    //        ihltah1[0].TavanFiyat = item.TavanFiyat;
+
+                    //    }
+                    //}
+                    string s = string.Format("UPDATE [FINSAT6{0}].[FINSAT6{0}].[IHLTAH] SET Onay = 1, Onaylayan='" + vUser.UserName + "', OnayTarih='{2}',DegisTarih='{2}'{3}  where ID = {1} AND Onay=0", "17", insertObj["ID"].ToString(), shortDate, sql);
+                    db.Database.ExecuteSqlCommand(s);
+                }
+                _Result.Status = true;
+                _Result.Message = "İşlem Başarılı ";
+
+            }
+            catch (Exception ex)
+            {
+
+                _Result.Status = false;
+                _Result.Message = "Hata Oluştu. ";
+
+            }
+            return Json(_Result, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult IhaleliRed(string Data)
+        {
+            Result _Result = new Result(true);
+            //  if (CheckPerm(Perms.TeminatOnay, PermTypes.Writing) == false) return null;
+            JArray parameters = JsonConvert.DeserializeObject<JArray>(Request["Data"]);
+            SqlExper sqlexper = new SqlExper(ConfigurationManager.ConnectionStrings["WMSConnection"].ConnectionString, "17");
+
+            try
+            {
+
+                foreach (JObject insertObj in parameters)
+                {
+                    string s = string.Format("DELETE FROM [FINSAT6{0}].[FINSAT6{0}].[IHLTAH] where ID = {1} AND Onay=0", "17", insertObj["ID"].ToString());
+                    db.Database.ExecuteSqlCommand(s);
+                }
+                _Result.Status = true;
+                _Result.Message = "İşlem Başarılı ";
+
+            }
+            catch (Exception ex)
+            {
+
+                _Result.Status = false;
+                _Result.Message = "Hata Oluştu. ";
+
+            }
+            return Json(_Result, JsonRequestBehavior.AllowGet);
+
+        }
+
     }
 }
