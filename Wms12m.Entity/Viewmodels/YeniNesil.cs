0e7c0e3a07044917947770f5f1bd5302b0fb3fb5
user <user@Y1>
2017-08-08 15:53:16 +0300

    yeni nesil onay

---
+++ Wms12m.Entity/Viewmodels/YeniNesil.cs
@@ -0,0 +1,18 @@
+﻿namespace Wms12m.Entity
+{
+    public class frmOnaySiparisList
+    {
+        public int ID { get; set; }
+        public string MalKodu { get; set; }
+        public string HesapKodu { get; set; }
+        public string Unvan { get; set; }
+        public string EvrakSeriNo { get; set; }
+        public string Depo { get; set; }
+        public decimal? Miktar { get; set; }
+        public decimal? BirimFiyat { get; set; }
+        public string DovizCinsi { get; set; }
+        public decimal? Tutar { get; set; }
+        public string Tarih { get; set; }
+        public string Kaydeden { get; set; }
+    }
+}
