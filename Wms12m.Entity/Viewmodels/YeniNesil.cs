5997b2978202b5e0a9bb64bcf171e1a978ef604d
user <user@Y1>
2017-08-08 18:35:45 +0300

    update

--- Wms12m.Entity/Viewmodels/YeniNesil.cs
+++ Wms12m.Entity/Viewmodels/YeniNesil.cs
@@ -3,7 +3,7 @@
     public class frmOnaySiparisList
     {
         public int ID { get; set; }
-        public string MalKodu { get; set; }
+        public int Cesit { get; set; }
         public string HesapKodu { get; set; }
         public string Unvan { get; set; }
         public string EvrakSeriNo { get; set; }
--- Wms12m.Presentation/Areas/YN/Controllers/OnayBekleyenlerController.cs
+++ Wms12m.Presentation/Areas/YN/Controllers/OnayBekleyenlerController.cs
@@ -20,12 +20,14 @@ namespace Wms12m.Presentation.Areas.YN.Controllers
         {
             using (YNSEntities dby = new YNSEntities())
             {
-                var list = dby.Database.SqlQuery<frmOnaySiparisList>(@"SELECT        YNS0TEST.STK002.STK002_Row_ID AS ID, YNS0TEST.STK002.STK002_MalKodu AS MalKodu, YNS0TEST.STK002.STK002_CariHesapKodu AS HesapKodu, YNS0TEST.CAR002.CAR002_Unvan1 AS Unvan, 
-                                                                                            YNS0TEST.STK002.STK002_EvrakSeriNo AS EvrakSeriNo, YNS0TEST.STK002.STK002_Depo AS Depo, YNS0TEST.STK002.STK002_Miktari AS Miktar, YNS0TEST.STK002.STK002_BirimFiyati AS BirimFiyat, 
-                                                                                            YNS0TEST.STK002.STK002_Tutari AS Tutar, YNS0TEST.STK002.STK002_DovizCinsi AS DovizCinsi, YNS0TEST.STK002.STK002_GirenKodu AS Kaydeden, CONVERT(VARCHAR(15), CAST(YNS0TEST.STK002.STK002_GirenTarih - 2 AS datetime), 104) AS Tarih
-                                                                FROM            YNS0TEST.STK002 INNER JOIN
-                                                                                            YNS0TEST.CAR002 ON YNS0TEST.STK002.STK002_CariHesapKodu = YNS0TEST.CAR002.CAR002_HesapKodu
-                                                                WHERE        (YNS0TEST.STK002.STK002_GC = 1) AND (YNS0TEST.STK002.STK002_SipDurumu = 0)").ToList();
+                var list = dby.Database.SqlQuery<frmOnaySiparisList>(@"SELECT        YNS0TEST.STK002.STK002_EvrakSeriNo AS EvrakSeriNo, YNS0TEST.STK002.STK002_CariHesapKodu AS HesapKodu, YNS0TEST.CAR002.CAR002_Unvan1 AS Unvan, COUNT(YNS0TEST.STK002.STK002_MalKodu) 
+                                                                                             AS Cesit, SUM(YNS0TEST.STK002.STK002_Miktari) AS Miktar, YNS0TEST.STK002.STK002_GirenKodu AS Kaydeden, CONVERT(VARCHAR(15), CAST(YNS0TEST.STK002.STK002_GirenTarih - 2 AS datetime), 104) 
+                                                                                             AS Tarih
+                                                                    FROM            YNS0TEST.STK002 INNER JOIN
+                                                                                             YNS0TEST.CAR002 ON YNS0TEST.STK002.STK002_CariHesapKodu = YNS0TEST.CAR002.CAR002_HesapKodu
+                                                                    WHERE        (YNS0TEST.STK002.STK002_GC = 1) AND (YNS0TEST.STK002.STK002_SipDurumu = 0)
+                                                                    GROUP BY YNS0TEST.STK002.STK002_CariHesapKodu, YNS0TEST.CAR002.CAR002_Unvan1, YNS0TEST.STK002.STK002_EvrakSeriNo, YNS0TEST.STK002.STK002_GirenKodu, CONVERT(VARCHAR(15), 
+                                                                                             CAST(YNS0TEST.STK002.STK002_GirenTarih - 2 AS datetime), 104)").ToList();
                 var json = new JavaScriptSerializer().Serialize(list);
                 return json;
             }
