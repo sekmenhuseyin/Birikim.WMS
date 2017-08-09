ad1fe1640f66649932ae4dc7ab2e0ad96f3ebb2d
user <user@Y1>
2017-08-09 18:13:57 +0300

    logs

--- Wms12m.Presentation/Areas/System/Controllers/LogsController.cs
+++ Wms12m.Presentation/Areas/System/Controllers/LogsController.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
+using Wms12m.Entity;
 
 namespace Wms12m.Presentation.Areas.System.Controllers
 {
@@ -25,6 +27,19 @@ namespace Wms12m.Presentation.Areas.System.Controllers
         {
             var list = db.GetAppLog().ToList();
             return View("App", list);
+        }
+        public JsonResult Delete(string ID)
+        {
+            try
+            {
+                var tip = Url.RequestContext.RouteData.Values["id"].ToString();
+                db.DeleteLog(tip, ID);
+                return Json(new Result(true), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new Result(false, ex.Message), JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
