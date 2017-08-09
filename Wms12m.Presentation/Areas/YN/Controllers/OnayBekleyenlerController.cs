1df353a48bbb7adc2e804e4bb36e68a6dab4fd42
user <user@Y1>
2017-08-09 14:21:25 +0300

    new page

--- Wms12m.Presentation/Areas/YN/Controllers/OnayBekleyenlerController.cs
+++ Wms12m.Presentation/Areas/YN/Controllers/OnayBekleyenlerController.cs
@@ -8,15 +8,17 @@ namespace Wms12m.Presentation.Areas.YN.Controllers
 {
     public class OnayBekleyenlerController : RootController
     {
-        // GET: YN/OnayBekleyenler
-        public ActionResult SiparisOnay()
+        /// <summary>
+        /// sipariş onayı bekleyenler sayfası
+        /// </summary>
+        public ActionResult Siparis()
         {
-            return View("SiparisOnay");
+            return View("Siparis");
         }
         /// <summary>
         /// sipariş onayı bekleyenler listesi
         /// </summary>
-        public string SiparisOnay_List()
+        public string Siparis_List()
         {
             using (YNSEntities dby = new YNSEntities())
             {
@@ -36,7 +38,50 @@ namespace Wms12m.Presentation.Areas.YN.Controllers
         /// liste detayı
         /// </summary>
         [HttpPost]
-        public JsonResult SiparisOnay_Details(string ID)
+        public JsonResult Siparis_Details(string ID)
+        {
+            using (YNSEntities dby = new YNSEntities())
+            {
+                var list = dby.Database.SqlQuery<frmOnaySiparisList>(@"SELECT        YNS0TEST.STK002.STK002_MalKodu AS MalKodu, YNS0TEST.STK002.STK002_CariHesapKodu AS HesapKodu, YNS0TEST.CAR002.CAR002_Unvan1 AS Unvan, 
+                                                                                            YNS0TEST.STK002.STK002_EvrakSeriNo AS EvrakSeriNo, YNS0TEST.STK002.STK002_Depo AS Depo, YNS0TEST.STK002.STK002_Miktari AS Miktar, YNS0TEST.STK002.STK002_BirimFiyati AS BirimFiyat, 
+                                                                                            YNS0TEST.STK002.STK002_Tutari AS Tutar, YNS0TEST.STK002.STK002_DovizCinsi AS DovizCinsi, YNS0TEST.STK002.STK002_GirenKodu AS Kaydeden, CONVERT(VARCHAR(15), CAST(YNS0TEST.STK002.STK002_GirenTarih - 2 AS datetime), 104) AS Tarih
+                                                                FROM            YNS0TEST.STK002 INNER JOIN
+                                                                                            YNS0TEST.CAR002 ON YNS0TEST.STK002.STK002_CariHesapKodu = YNS0TEST.CAR002.CAR002_HesapKodu
+                                                                WHERE        (YNS0TEST.STK002.STK002_GC = 1) AND (YNS0TEST.STK002.STK002_SipDurumu = 0) AND YNS0TEST.STK002.STK002_EvrakSeriNo = '" + ID + "'").ToList();
+                return Json(list, JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>
+        /// sipariş onayı bekleyenler sayfası
+        /// </summary>
+        public ActionResult Transfer()
+        {
+            return View("Transfer");
+        }
+        /// <summary>
+        /// sipariş onayı bekleyenler listesi
+        /// </summary>
+        public string Transfer_List()
+        {
+            using (YNSEntities dby = new YNSEntities())
+            {
+                var list = dby.Database.SqlQuery<frmOnaySiparisList>(@"SELECT        YNS0TEST.STK002.STK002_EvrakSeriNo AS EvrakSeriNo, YNS0TEST.STK002.STK002_CariHesapKodu AS HesapKodu, YNS0TEST.CAR002.CAR002_Unvan1 AS Unvan, COUNT(YNS0TEST.STK002.STK002_MalKodu) 
+                                                                                             AS Cesit, SUM(YNS0TEST.STK002.STK002_Miktari) AS Miktar, YNS0TEST.STK002.STK002_GirenKodu AS Kaydeden, CONVERT(VARCHAR(15), CAST(YNS0TEST.STK002.STK002_GirenTarih - 2 AS datetime), 104) 
+                                                                                             AS Tarih
+                                                                    FROM            YNS0TEST.STK002 INNER JOIN
+                                                                                             YNS0TEST.CAR002 ON YNS0TEST.STK002.STK002_CariHesapKodu = YNS0TEST.CAR002.CAR002_HesapKodu
+                                                                    WHERE        (YNS0TEST.STK002.STK002_GC = 1) AND (YNS0TEST.STK002.STK002_SipDurumu = 0)
+                                                                    GROUP BY YNS0TEST.STK002.STK002_CariHesapKodu, YNS0TEST.CAR002.CAR002_Unvan1, YNS0TEST.STK002.STK002_EvrakSeriNo, YNS0TEST.STK002.STK002_GirenKodu, CONVERT(VARCHAR(15), 
+                                                                                             CAST(YNS0TEST.STK002.STK002_GirenTarih - 2 AS datetime), 104)").ToList();
+                var json = new JavaScriptSerializer().Serialize(list);
+                return json;
+            }
+        }
+        /// <summary>
+        /// liste detayı
+        /// </summary>
+        [HttpPost]
+        public JsonResult Transfer_Details(string ID)
         {
             using (YNSEntities dby = new YNSEntities())
             {
