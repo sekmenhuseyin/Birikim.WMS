3fe6cddfeec9be5d0014905743f6043b3ff61242
user <user@Y1>
2017-08-09 14:59:21 +0300

    transfer

--- Wms12m.Entity/Viewmodels/YeniNesil.cs
+++ Wms12m.Entity/Viewmodels/YeniNesil.cs
@@ -1,5 +1,8 @@
 ﻿namespace Wms12m.Entity
 {
+    /// <summary>
+    /// onay bekleyen sipariş listesi
+    /// </summary>
     public class frmOnaySiparisList
     {
         public int ID { get; set; }
@@ -15,5 +18,22 @@
         public decimal? Tutar { get; set; }
         public string Tarih { get; set; }
         public string Kaydeden { get; set; }
+    }
+    /// <summary>
+    /// onay bekleyen transfer listesi
+    /// </summary>
+    public class frmOnayTransferList
+    {
+        public int ID { get; set; }
+        public string TransferNo { get; set; }
+        public int TransferTarihi { get; set; }
+        public string Tarih { get; set; }
+        public string MalKodu { get; set; }
+        public decimal Miktar { get; set; }
+        public string Birim { get; set; }
+        public string CikisDepo { get; set; }
+        public string GirisDepo { get; set; }
+        public string Kaydeden { get; set; }
+        public int KayitTarih { get; set; }
     }
 }
--- Wms12m.Presentation/Areas/YN/Controllers/OnayBekleyenlerController.cs
+++ Wms12m.Presentation/Areas/YN/Controllers/OnayBekleyenlerController.cs
@@ -52,27 +52,23 @@ namespace Wms12m.Presentation.Areas.YN.Controllers
             }
         }
         /// <summary>
-        /// sipariş onayı bekleyenler sayfası
+        /// transfer onayı bekleyenler sayfası
         /// </summary>
         public ActionResult Transfer()
         {
             return View("Transfer");
         }
         /// <summary>
-        /// sipariş onayı bekleyenler listesi
+        /// transfer onayı bekleyenler listesi
         /// </summary>
         public string Transfer_List()
         {
             using (YNSEntities dby = new YNSEntities())
             {
-                var list = dby.Database.SqlQuery<frmOnaySiparisList>(@"SELECT        YNS0TEST.STK002.STK002_EvrakSeriNo AS EvrakSeriNo, YNS0TEST.STK002.STK002_CariHesapKodu AS HesapKodu, YNS0TEST.CAR002.CAR002_Unvan1 AS Unvan, COUNT(YNS0TEST.STK002.STK002_MalKodu) 
-                                                                                             AS Cesit, SUM(YNS0TEST.STK002.STK002_Miktari) AS Miktar, YNS0TEST.STK002.STK002_GirenKodu AS Kaydeden, CONVERT(VARCHAR(15), CAST(YNS0TEST.STK002.STK002_GirenTarih - 2 AS datetime), 104) 
-                                                                                             AS Tarih
-                                                                    FROM            YNS0TEST.STK002 INNER JOIN
-                                                                                             YNS0TEST.CAR002 ON YNS0TEST.STK002.STK002_CariHesapKodu = YNS0TEST.CAR002.CAR002_HesapKodu
-                                                                    WHERE        (YNS0TEST.STK002.STK002_GC = 1) AND (YNS0TEST.STK002.STK002_SipDurumu = 0)
-                                                                    GROUP BY YNS0TEST.STK002.STK002_CariHesapKodu, YNS0TEST.CAR002.CAR002_Unvan1, YNS0TEST.STK002.STK002_EvrakSeriNo, YNS0TEST.STK002.STK002_GirenKodu, CONVERT(VARCHAR(15), 
-                                                                                             CAST(YNS0TEST.STK002.STK002_GirenTarih - 2 AS datetime), 104)").ToList();
+                var list = dby.Database.SqlQuery<frmOnayTransferList>(@"SELECT        TransferNo, Kaydeden, CONVERT(VARCHAR(15), CAST(TransferTarihi - 2 AS datetime), 104) AS Tarih
+                                                                    FROM            YNS0TEST.TransferDepo
+                                                                    WHERE        (OnayDurumu = 0)
+                                                                    GROUP BY TransferNo, Kaydeden, CONVERT(VARCHAR(15), CAST(TransferTarihi - 2 AS datetime), 104)").ToList();
                 var json = new JavaScriptSerializer().Serialize(list);
                 return json;
             }
@@ -85,12 +81,9 @@ namespace Wms12m.Presentation.Areas.YN.Controllers
         {
             using (YNSEntities dby = new YNSEntities())
             {
-                var list = dby.Database.SqlQuery<frmOnaySiparisList>(@"SELECT        YNS0TEST.STK002.STK002_MalKodu AS MalKodu, YNS0TEST.STK002.STK002_CariHesapKodu AS HesapKodu, YNS0TEST.CAR002.CAR002_Unvan1 AS Unvan, 
-                                                                                            YNS0TEST.STK002.STK002_EvrakSeriNo AS EvrakSeriNo, YNS0TEST.STK002.STK002_Depo AS Depo, YNS0TEST.STK002.STK002_Miktari AS Miktar, YNS0TEST.STK002.STK002_BirimFiyati AS BirimFiyat, 
-                                                                                            YNS0TEST.STK002.STK002_Tutari AS Tutar, YNS0TEST.STK002.STK002_DovizCinsi AS DovizCinsi, YNS0TEST.STK002.STK002_GirenKodu AS Kaydeden, CONVERT(VARCHAR(15), CAST(YNS0TEST.STK002.STK002_GirenTarih - 2 AS datetime), 104) AS Tarih
-                                                                FROM            YNS0TEST.STK002 INNER JOIN
-                                                                                            YNS0TEST.CAR002 ON YNS0TEST.STK002.STK002_CariHesapKodu = YNS0TEST.CAR002.CAR002_HesapKodu
-                                                                WHERE        (YNS0TEST.STK002.STK002_GC = 1) AND (YNS0TEST.STK002.STK002_SipDurumu = 0) AND YNS0TEST.STK002.STK002_EvrakSeriNo = '" + ID + "'").ToList();
+                var list = dby.Database.SqlQuery<frmOnayTransferList>(@"SELECT        ID, TransferNo, TransferTarihi, SiraNo, MalKodu, Miktar, Birim, CikisDepo, GirisDepo, TalepEden, OnayDurumu, Kaydeden, KayitTarih
+                                                                    FROM            YNS0TEST.TransferDepo
+                                                                    WHERE        TransferNo = '" + ID + "'").ToList();
                 return Json(list, JsonRequestBehavior.AllowGet);
             }
         }
