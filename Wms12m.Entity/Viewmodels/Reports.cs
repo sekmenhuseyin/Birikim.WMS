4747afc7e5616dfb38148b3d58f631abd357588a
Derviş Akdeniz <d.akdeniz@12mconsulting.com.tr>
2017-08-30 14:10:16 +0300

    raporlar

--- Wms12m.Entity/Viewmodels/Reports.cs
+++ Wms12m.Entity/Viewmodels/Reports.cs
@@ -244,6 +244,7 @@ namespace Wms12m.Entity
     public class KampanyaliSatisRaporu
     {
         public string Chk { get; set; }
+        public string Unvan { get; set; }
         public string EvrakNo { get; set; }
         public string ComfortBirim { get; set; }
         public string ExculusiveBirim { get; set; }
@@ -293,6 +294,7 @@ namespace Wms12m.Entity
     public class KampanyaSiparisDetay
     {
         public string Chk { get; set; }
+        public string Unvan { get; set; }
         public string EvrakNo { get; set; }
         public DateTime Tarih { get; set; }
         public decimal Tutar { get; set; }
