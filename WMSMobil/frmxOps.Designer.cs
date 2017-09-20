e9e16ebdac7e70e9f7d65b454d031fe4614ae5f9
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-09-20 10:36:43 +0300

    term

--- WMSMobil/frmxOps.Designer.cs
+++ WMSMobil/frmxOps.Designer.cs
@@ -35,7 +35,6 @@
             this.txtBarkod = new System.Windows.Forms.TextBox();
             this.btnEkle = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.txtEvrakno = new System.Windows.Forms.TextBox();
@@ -58,6 +57,7 @@
             this.panelAlt = new System.Windows.Forms.Panel();
             this.btnBack = new System.Windows.Forms.Button();
             this.btnKaydet = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
             this.panelUst.SuspendLayout();
             this.panelOrta.SuspendLayout();
             this.panelAlt.SuspendLayout();
@@ -134,17 +134,6 @@
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(70, 21);
             this.label7.Text = "Raf Okut";
-            // 
-            // label14
-            // 
-            this.label14.BackColor = System.Drawing.Color.CadetBlue;
-            this.label14.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
-            this.label14.ForeColor = System.Drawing.SystemColors.Window;
-            this.label14.Location = new System.Drawing.Point(3, 47);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(110, 21);
-            this.label14.Text = "Makara Okut";
-            this.label14.Visible = false;
             // 
             // label4
             // 
@@ -360,6 +349,17 @@
             this.btnKaydet.Text = "Kaydet";
             this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
             // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.Color.CadetBlue;
+            this.label14.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.label14.ForeColor = System.Drawing.SystemColors.Window;
+            this.label14.Location = new System.Drawing.Point(3, 47);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(110, 21);
+            this.label14.Text = "Makara Okut";
+            this.label14.Visible = false;
+            // 
             // frmxOps
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
@@ -409,8 +409,8 @@
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnUygula;
         private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label14;
         private System.Windows.Forms.TextBox txtMakaraBarkod;
         private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
     }
 }
