e1e22076923c2227d8817f0f0ce2a07866bf875f
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-09-20 16:04:11 +0300

    term

--- WMSMobil/frmxPackage.cs
+++ WMSMobil/frmxPackage.cs
@@ -29,7 +29,7 @@ namespace WMSMobil
             Barkod.DeviceType = Symbol.Barcode2.DEVICETYPES.FIRSTAVAILABLE;
             try
             {
-                //Barkod.EnableScanner = true;
+                Barkod.EnableScanner = true;
             }
             catch (Exception)
             {
