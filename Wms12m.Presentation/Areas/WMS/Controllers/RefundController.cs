8ad9ac297638b559c8dafae27ba9663cce6e0024
Derviş Akdeniz <dervis@12mconsulting.com.tr>
2017-10-03 19:05:10 +0300

    aa

--- Wms12m.Presentation/Areas/WMS/Controllers/RefundController.cs
+++ Wms12m.Presentation/Areas/WMS/Controllers/RefundController.cs
@@ -53,12 +53,24 @@ namespace Wms12m.Presentation.Areas.WMS.Controllers
             if (StartDate > EndDate) return null;
             //perm kontrol
             if (CheckPerm(Perms.GenelSipariş, PermTypes.Reading) == false) return null;
-            string sql = String.Format("SELECT '{0}' as SirketID, FINSAT6{0}.FINSAT6{0}.SPI.EvrakNo, FINSAT6{0}.FINSAT6{0}.SPI.Tarih, FINSAT6{0}.FINSAT6{0}.SPI.Chk, FINSAT6{0}.FINSAT6{0}.CHK.Unvan1 AS Unvan, FINSAT6{0}.FINSAT6{0}.CHK.GrupKod, FINSAT6{0}.FINSAT6{0}.CHK.FaturaAdres3 AS FaturaAdres, FINSAT6{0}.FINSAT6{0}.MFK.Aciklama, COUNT(FINSAT6{0}.FINSAT6{0}.SPI.MalKodu) AS Çeşit, SUM(FINSAT6{0}.FINSAT6{0}.SPI.BirimMiktar - FINSAT6{0}.FINSAT6{0}.SPI.TeslimMiktar - FINSAT6{0}.FINSAT6{0}.SPI.KapatilanMiktar) AS Miktar, MIN(FINSAT6{0}.FINSAT6{0}.SPI.KayitSaat) as Saat " +
-                                    "FROM FINSAT6{0}.FINSAT6{0}.SPI WITH(NOLOCK) INNER JOIN FINSAT6{0}.FINSAT6{0}.STK WITH(NOLOCK) ON FINSAT6{0}.FINSAT6{0}.SPI.MalKodu = FINSAT6{0}.FINSAT6{0}.STK.MalKodu INNER JOIN FINSAT6{0}.FINSAT6{0}.MFK WITH(NOLOCK) ON FINSAT6{0}.FINSAT6{0}.SPI.EvrakNo = FINSAT6{0}.FINSAT6{0}.MFK.EvrakNo AND FINSAT6{0}.FINSAT6{0}.SPI.Tarih = FINSAT6{0}.FINSAT6{0}.MFK.EvrakTarih AND FINSAT6{0}.FINSAT6{0}.SPI.Chk = FINSAT6{0}.FINSAT6{0}.MFK.HesapKod AND FINSAT6{0}.FINSAT6{0}.SPI.KynkEvrakTip = FINSAT6{0}.FINSAT6{0}.MFK.KynkEvrakTip INNER JOIN FINSAT6{0}.FINSAT6{0}.CHK WITH(NOLOCK) ON FINSAT6{0}.FINSAT6{0}.SPI.Chk = FINSAT6{0}.FINSAT6{0}.CHK.HesapKodu " +
-                                    "WHERE (FINSAT6{0}.FINSAT6{0}.SPI.Tarih >= '{2}') AND (FINSAT6{0}.FINSAT6{0}.SPI.Tarih <= '{3}') AND (FINSAT6{0}.FINSAT6{0}.SPI.Depo = '{1}') AND (FINSAT6{0}.FINSAT6{0}.SPI.SiparisDurumu = 0) AND (FINSAT6{0}.FINSAT6{0}.SPI.KynkEvrakTip = 62) AND (FINSAT6{0}.FINSAT6{0}.SPI.Kod10 IN ('Terminal', 'Onaylandı')) AND (FINSAT6{0}.FINSAT6{0}.STK.Kod1 <> 'KKABLO') AND " +
-                                    "(FINSAT6{0}.FINSAT6{0}.SPI.BirimMiktar - FINSAT6{0}.FINSAT6{0}.SPI.TeslimMiktar - FINSAT6{0}.FINSAT6{0}.SPI.KapatilanMiktar) > 0 AND " +
-                                    "FINSAT6{0}.FINSAT6{0}.SPI.ROW_ID NOT IN (SELECT BIRIKIM.wms.IRS_Detay.KynkSiparisID FROM BIRIKIM.wms.IRS_Detay INNER JOIN BIRIKIM.wms.GorevIRS ON BIRIKIM.wms.IRS_Detay.IrsaliyeID = BIRIKIM.wms.GorevIRS.IrsaliyeID INNER JOIN BIRIKIM.wms.Gorev ON BIRIKIM.wms.GorevIRS.GorevID = BIRIKIM.wms.Gorev.ID WHERE (BIRIKIM.wms.Gorev.DurumID = 9 OR BIRIKIM.wms.Gorev.DurumID = 11) AND (NOT(BIRIKIM.wms.IRS_Detay.KynkSiparisID IS NULL)) GROUP BY BIRIKIM.wms.IRS_Detay.KynkSiparisID) " +
-                                    "GROUP BY FINSAT6{0}.FINSAT6{0}.SPI.EvrakNo, FINSAT6{0}.FINSAT6{0}.SPI.Tarih, FINSAT6{0}.FINSAT6{0}.SPI.Chk, FINSAT6{0}.FINSAT6{0}.CHK.Unvan1, FINSAT6{0}.FINSAT6{0}.CHK.GrupKod, FINSAT6{0}.FINSAT6{0}.CHK.FaturaAdres3, FINSAT6{0}.FINSAT6{0}.MFK.Aciklama", Sirket, DepoID.ToString(), StartDate.ToOADateInt(), EndDate.ToOADateInt());
+            string sql = String.Format(@"SELECT 
+'{0}' as SirketID, 
+STI.EvrakNo, 
+STI.Tarih, 
+STI.Chk, 
+CHK.Unvan1 AS Unvan, 
+CHK.GrupKod, 
+CHK.FaturaAdres3 AS FaturaAdres, 
+MFK.Aciklama, 
+COUNT(STI.MalKodu) AS Çeşit, 
+SUM(STI.miktar) AS Miktar, 
+MIN(STI.KayitSaat) as Saat 
+FROM FINSAT6{0}.FINSAT6{0}.STI WITH(NOLOCK) 
+INNER JOIN FINSAT6{0}.FINSAT6{0}.STK WITH(NOLOCK) ON STI.MalKodu = STK.MalKodu 
+INNER JOIN FINSAT6{0}.FINSAT6{0}.MFK WITH(NOLOCK) ON STI.EvrakNo = MFK.EvrakNo AND STI.Tarih = MFK.EvrakTarih AND STI.Chk = MFK.HesapKod AND STI.KynkEvrakTip = MFK.KynkEvrakTip 
+INNER JOIN FINSAT6{0}.FINSAT6{0}.CHK WITH(NOLOCK) ON STI.Chk = CHK.HesapKodu 
+WHERE (STI.Tarih >= '{2}') AND (STI.Tarih <= '{3}') AND (STI.Depo = '{1}') AND (STI.KynkEvrakTip = 4) AND (STI.Chk='{4}')
+GROUP BY STI.EvrakNo, STI.Tarih, STI.Chk, CHK.Unvan1, CHK.GrupKod, CHK.FaturaAdres3, MFK.Aciklama", Sirket, DepoID.ToString(), StartDate.ToOADateInt(), EndDate.ToOADateInt(), CHK);
 
             //return list
             ViewBag.Depo = DepoID;
@@ -74,6 +86,19 @@ namespace Wms12m.Presentation.Areas.WMS.Controllers
                 Logger(ex, "Refund/GetSiparis");
                 return PartialView("SiparisList", new List<frmSiparisler>());
             }
+        }
+
+        /// <summary>
+        /// evrak noya ait mallar
+        /// </summary>
+        [HttpPost]
+        public JsonResult Details(string ID)
+        {
+            if (CheckPerm(Perms.GenelSipariş, PermTypes.Reading) == false) return Json(new Result(false, "Yetkiniz yok"), JsonRequestBehavior.AllowGet);
+            string[] tmp = ID.Split('-');
+            string sql = String.Format("FINSAT6{0}.wms.AlimIptalDetail @DepoKodu = '{1}', @EvrakNo = '{2}' @CHK='{3}'", tmp[0], tmp[1], tmp[2], tmp[3]);
+            var list = db.Database.SqlQuery<frmSiparisMalzeme>(sql).ToList();
+            return Json(list, JsonRequestBehavior.AllowGet);
         }
     }
 
