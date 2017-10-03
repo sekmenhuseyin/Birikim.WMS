b2ade66e8ab78d10e9e401274cde61f503a0dd02
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-10-03 14:46:15 +0300

    update

--- Wms12m.Presentation/Properties/AssemblyInfo.cs
+++ Wms12m.Presentation/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Wms12m.Presentation")]
+[assembly: AssemblyTitle("12M ERP ve Görev")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("12M Consulting")]
-[assembly: AssemblyProduct("Wms12m.Presentation")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("12M ERP ve Görev")]
+[assembly: AssemblyCopyright("Copyright ©  2016 - 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
