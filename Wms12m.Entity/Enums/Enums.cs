c0e87e3f0badf302135e6a9a769c1582ca32de60
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-10-10 18:59:27 +0300

    chart aylık

--- Wms12m.Entity/Enums/Enums.cs
+++ Wms12m.Entity/Enums/Enums.cs
@@ -84,8 +84,8 @@
         ptPalet = 69,
         Bakır = 70,
         KüçükRaf = 71,
-        Alımdanİade=72,
-        Satıştanİade=73
+        Alımdanİade = 72,
+        Satıştanİade = 73
     }
     /// <summary>
     /// combo item names
@@ -198,5 +198,24 @@
         TodoGörevler,
         TerminalAlimdanİade,
         TerminalSatıştanİade
+    }
+    /// <summary>
+    /// aylar
+    /// </summary>
+    public enum Aylar
+    {
+        Tümü = 0,
+        Ocak = 1,
+        Şubat = 2,
+        Mart = 3,
+        Nisan = 4,
+        Mayıs = 5,
+        Haziran = 6,
+        Temmuz = 7,
+        Ağustos = 8,
+        Eylül = 9,
+        Ekim = 10,
+        Kasım = 11,
+        Aralık = 12
     }
 }
