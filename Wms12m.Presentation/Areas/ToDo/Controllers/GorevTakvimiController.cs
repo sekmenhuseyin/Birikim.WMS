beca02fae2dff0de800a85cf12e585ef37c491a3
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-10-11 13:07:56 +0300

    takvim

--- Wms12m.Presentation/Areas/ToDo/Controllers/GorevTakvimiController.cs
+++ Wms12m.Presentation/Areas/ToDo/Controllers/GorevTakvimiController.cs
@@ -12,8 +12,29 @@ namespace Wms12m.Presentation.Areas.ToDo.Controllers
         public ActionResult Index()
         {
             if (CheckPerm(Perms.TodoTakvim, PermTypes.Reading) == false) return Redirect("/");
+            ViewBag.UserName = vUser.UserName;
+            ViewBag.Yetki = vUser.RoleName;
+            if (vUser.RoleName == "Admin" || vUser.RoleName == " ")
+                ViewBag.UserID = new SelectList(Persons.GetList(), "Kod", "AdSoyad");
             var list = db.GorevlerCalismas.Where(m => m.Kaydeden == vUser.UserName).GroupBy(m => m.Tarih).Select(m => new frmGorevlerCalismalar { Tarih = m.Key, Sure = m.Sum(n => n.Sure) }).ToList();
             return View(list);
+        }
+        /// <summary>
+        /// liste
+        /// </summary>
+        public PartialViewResult List(string UserName)
+        {
+            return PartialView();
+        }
+        /// <summary>
+        /// ayrıntılar
+        /// </summary>
+        [HttpPost]
+        public PartialViewResult Details(int ID, string User)
+        {
+            var Tarih = ID.FromOaDate();
+            var list = db.GorevlerCalismas.Where(m => m.Tarih == Tarih && m.Kaydeden == User).OrderByDescending(m => m.ID).ToList();
+            return PartialView("Duty_Details", list);
         }
     }
 }
