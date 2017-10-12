719868b1f63ae010776d043bfd689ace8b3cbf1a
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-10-12 19:04:53 +0300

    takvim

--- Wms12m.Presentation/Controllers/CalendarController.cs
+++ Wms12m.Presentation/Controllers/CalendarController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Wms12m.Entity.Models;
 
 namespace Wms12m.Presentation.Controllers
 {
@@ -11,7 +10,27 @@ namespace Wms12m.Presentation.Controllers
         // GET: Calendar
         public ActionResult Index()
         {
-            return View();
+            var liste = db.Etkinliks.Where(m => m.Tekrarlayan == false).ToList();
+            var tekrarlayan = db.Etkinliks.Where(m => m.Tekrarlayan == true).ToList();
+            foreach (var item in tekrarlayan)
+            {
+                var fark = DateTime.Today.Year - item.Tarih.Year + 1;
+                for (int i = 0; i <= fark; i++)
+                {
+                    var item2 = new Etkinlik()
+                    {
+                        ID = item.ID,
+                        Username = item.Username,
+                        TatilTipi = item.TatilTipi,
+                        Tarih = item.Tarih.AddYears(i),
+                        Aciklama = item.Aciklama + " (" + i + ". Tekrar)",
+                        Tekrarlayan = item.Tekrarlayan,
+                        ComboItem_Name = item.ComboItem_Name
+                    };
+                    liste.Add(item2);
+                }
+            }
+            return View("Index", liste);
         }
     }
 }
