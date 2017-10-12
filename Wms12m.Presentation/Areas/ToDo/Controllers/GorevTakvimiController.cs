a4940827c1f8b52bef66026213c624ef9d6f4ebe
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-10-12 10:33:11 +0300

    tatil günleri

--- Wms12m.Presentation/Areas/ToDo/Controllers/GorevTakvimiController.cs
+++ Wms12m.Presentation/Areas/ToDo/Controllers/GorevTakvimiController.cs
@@ -27,7 +27,16 @@ namespace Wms12m.Presentation.Areas.ToDo.Controllers
             if (vUser.RoleName == "Admin" || vUser.RoleName == " ")
                 ViewBag.UserID = new SelectList(Persons.GetList(), "Kod", "AdSoyad");
             var list = db.GorevlerCalismas.Where(m => m.Kaydeden == UserName).GroupBy(m => m.Tarih).Select(m => new frmGorevlerCalismalar { Tarih = m.Key, Sure = m.Sum(n => n.Sure) }).ToList();
+            ViewBag.Tatil= db.Tatils.Where(m=>m.user).ToList();
             return PartialView("List", list);
+        }
+        /// <summary>
+        /// tatil liste
+        /// </summary>
+        public PartialViewResult ListTatil(string UserName)
+        {
+            var list = db.Tatils.OrderByDescending(m => m.Tarih).ToList();
+            return PartialView("ListTatil", list);
         }
         /// <summary>
         /// ayrıntılar
