e78dadc885d6c883fdfe0cb851f421c79c9611b0
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-10-13 17:59:35 +0300

    bildirimler

--- Wms12m.Presentation/Controllers/InboxController.cs
+++ Wms12m.Presentation/Controllers/InboxController.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Wms12m.Entity;
 
 namespace Wms12m.Presentation.Controllers
 {
     public class InboxController : RootController
     {
-        // GET: Inbox
+        /// <summary>
+        /// gelen kutusu
+        /// </summary>
         public ActionResult Index()
         {
             return View();
+        }
+        /// <summary>
+        /// gelen bildirimler
+        /// </summary>
+        public ActionResult Notifications()
+        {
+            return View();
+        }
+        /// <summary>
+        /// bildiri listesi
+        /// </summary>
+        public JsonResult NotificationList()
+        {
+            return Json(new Result(false, 0, "yetki hatası"), JsonRequestBehavior.AllowGet);
         }
     }
 }
