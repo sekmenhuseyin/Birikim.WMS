d123ccf7d4619fcbdeecb12f416202cfba6c9288
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-10-16 16:42:01 +0300

    dbx connections

---
+++ Wms12m.Presentation/Helper/Statics.cs
@@ -0,0 +1,17 @@
+﻿using Wms12m.Entity.Models;
+
+namespace Wms12m
+{
+    public class Statics
+    {
+        public static void PutUsersOffline()
+        {
+            using (var db = new WMSEntities())
+            {
+                foreach (var connection in db.Connections)
+                    connection.IsOnline = false;
+                db.SaveChanges();
+            }
+        }
+    }
+}
