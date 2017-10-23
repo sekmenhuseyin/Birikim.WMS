896f649673d083c6509232c888db10bb11558ed8
Hüseyin Sekmenoğlu <h.sekmenoglu@12mconsulting.com.tr>
2017-10-23 09:40:57 +0300

    help

--- Wms12m.Presentation/Controllers/HelpController.cs
+++ Wms12m.Presentation/Controllers/HelpController.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
+using Wms12m.Entity;
+using Wms12m.Entity.Models;
 
 namespace Wms12m.Presentation.Controllers
 {
@@ -10,16 +13,73 @@ namespace Wms12m.Presentation.Controllers
         /// </summary>
         public ActionResult Index()
         {
-            var liste = db.FAQs.ToList();
-            return View("Index", liste);
+            return View("Index");
         }
         /// <summary>
         /// yardım sayfası listesi
         /// </summary>
-        public PartialViewResult List()
+        public PartialViewResult List(string search)
         {
-            var liste = db.FAQs.ToList();
-            return PartialView("List", liste);
+            var liste = db.FAQs.Where(m => m.ID > 0);
+            if (search != "" && search != null)
+                liste = liste.Where(m => m.Title.Contains(search) || m.Detail.Contains(search));
+            return PartialView("List", liste.ToList());
+        }
+        /// <summary>
+        /// yeni sayfası
+        /// </summary>
+        public PartialViewResult New()
+        {
+            ViewBag.TopicTypeID = new SelectList(ComboSub.GetList(Combos.FaqTopics.ToInt32()), "ID", "Name");
+            return PartialView(new FAQ());
+        }
+        /// <summary>
+        /// kaydet
+        /// </summary>
+        [HttpPost, ValidateAntiForgeryToken]
+        public JsonResult Save(FAQ satir)
+        {
+            if (ModelState.IsValid)
+            {
+                if (satir.ID == 0)
+                {
+                    db.FAQs.Add(satir);
+                }
+                else
+                {
+                    var tbl = db.FAQs.Where(m => m.ID == satir.ID).FirstOrDefault();
+                    tbl.TopicTypeID = satir.TopicTypeID;
+                    tbl.Title = satir.Title;
+                    tbl.Detail = satir.Detail;
+                }
+                try
+                {
+                    db.SaveChanges();
+                    return Json(new Result(true, satir.ID), JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            return Json(new Result(false, "Hata oldu"), JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// sil
+        /// </summary>
+        public JsonResult Delete(string Id)
+        {
+            var tbl = db.FAQs.Find(Id.ToInt32());
+            db.FAQs.Remove(tbl);
+            try
+            {
+                db.SaveChanges();
+                return Json(new Result(true, Id.ToInt32()), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                return Json(new Result(false, "Silme işlemi gerçekleştirilemedi."), JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
