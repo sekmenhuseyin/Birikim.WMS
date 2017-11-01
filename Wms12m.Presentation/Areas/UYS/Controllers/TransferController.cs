1557ba43da995229de56cb7aaa7d62b644adce5b
HS <h.sekmenoglu@12mconsulting.com.tr>
2017-11-01 17:41:16 +0300

    uys area

---
+++ Wms12m.Presentation/Areas/UYS/Controllers/TransferController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Areas.UYS.Controllers
+{
+    public class TransferController : RootController
+    {
+        /// <summary>
+        /// transfer planlama
+        /// </summary>
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ Wms12m.Presentation/Areas/UYS/UYSAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Areas.UYS
+{
+    public class UYSAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "UYS";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "UYS_default",
+                "UYS/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "Wms12m.Presentation.Areas.UYS.Controllers" }
+            );
+        }
+    }
+}
