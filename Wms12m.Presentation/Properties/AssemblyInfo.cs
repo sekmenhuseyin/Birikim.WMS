33a0743d2d2d6bcff31c0fb646cf06e7c49e66d0
HS <h.sekmenoglu@12mconsulting.com.tr>
2017-11-13 09:23:47 +0300

    version

--- Wms12m.Presentation/Properties/AssemblyInfo.cs
+++ Wms12m.Presentation/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.71.110")]
-[assembly: AssemblyFileVersion("1.71.110")]
+[assembly: AssemblyVersion("1.71.113")]
+[assembly: AssemblyFileVersion("1.71.113")]
 [assembly: NeutralResourcesLanguage("tr-TR")]
 
