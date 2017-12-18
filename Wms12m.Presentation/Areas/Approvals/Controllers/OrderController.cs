a0d3494aef6f037c3f09f0dca285385f890acb8f
HS <h.sekmenoglu@12mconsulting.com.tr>
2017-12-18 12:12:22 +0300

    sipariş

--- Wms12m.Presentation/Areas/Approvals/Controllers/OrderController.cs
+++ Wms12m.Presentation/Areas/Approvals/Controllers/OrderController.cs
@@ -51,38 +51,34 @@ namespace Wms12m.Presentation.Areas.Approvals.Controllers
             if (CheckPerm(Perms.SiparişOnaylama, PermTypes.Writing) == false) return Json(new Result(false, "Yetkiniz yok"), JsonRequestBehavior.AllowGet);
             var _Result = new Result(true);
             var parameters = JsonConvert.DeserializeObject<JArray>(Request["Data"]);
-            using (var dbContextTransaction = db.Database.BeginTransaction())
+            string sql = "";
+            foreach (string insertObj in parameters)
             {
-                foreach (string insertObj in parameters)
-                {
-                    var OTip = db.Database.SqlQuery<short>(string.Format("SELECT OnayTip FROM FINSAT6{0}.FINSAT6{0}.SiparisOnay WHERE EvrakNo='{1}'", vUser.SirketKodu, insertObj)).FirstOrDefault();
-                    if (OnayTip == 3 && OnaylandiMi == true)//GMOnay
-                    { db.Database.ExecuteSqlCommand(string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 3, 1, OTip)); }
-                    else if (OnayTip == 2 && OnaylandiMi == true)//SPGMYOnay
-                    { db.Database.ExecuteSqlCommand(string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 2, 1, OTip)); }
-                    else if (OnayTip == 1 && OnaylandiMi == true)//SMOnay
-                    { db.Database.ExecuteSqlCommand(string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 1, 1, OTip)); }
-                    else if (OnayTip == 3 && OnaylandiMi == false)//GMRet
-                    { db.Database.ExecuteSqlCommand(string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 3, 0, OTip)); }
-                    else if (OnayTip == 2 && OnaylandiMi == false)//SPGMYRet
-                    { db.Database.ExecuteSqlCommand(string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 2, 0, OTip)); }
-                    else if (OnayTip == 1 && OnaylandiMi == false)//SMRet
-                    { db.Database.ExecuteSqlCommand(string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 1, 0, OTip)); }
-                }
+                var OTip = db.Database.SqlQuery<short>(string.Format("SELECT OnayTip FROM FINSAT6{0}.FINSAT6{0}.SiparisOnay WHERE EvrakNo='{1}'", vUser.SirketKodu, insertObj)).FirstOrDefault();
+                if (OnayTip == 3 && OnaylandiMi == true)//GMOnay
+                { sql = string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 3, 1, OTip); }
+                else if (OnayTip == 2 && OnaylandiMi == true)//SPGMYOnay
+                { sql = string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 2, 1, OTip); }
+                else if (OnayTip == 1 && OnaylandiMi == true)//SMOnay
+                { sql = string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 1, 1, OTip); }
+                else if (OnayTip == 3 && OnaylandiMi == false)//GMRet
+                { sql = string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 3, 0, OTip); }
+                else if (OnayTip == 2 && OnaylandiMi == false)//SPGMYRet
+                { sql = string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 2, 0, OTip); }
+                else if (OnayTip == 1 && OnaylandiMi == false)//SMRet
+                { sql = string.Format("[FINSAT6{0}].[wms].[SP_SiparisOnay] @EvrakNo = '{1}',@Kullanici = '{2}',@OnaylayanTip={3},@OnaylandiMi={4},@OnayTip={5}", vUser.SirketKodu, insertObj, vUser.UserName, 1, 0, OTip); }
+            }
 
-                try
-                {
-                    db.SaveChanges();
-                    dbContextTransaction.Commit();
-
-                    _Result.Status = true;
-                    _Result.Message = "İşlem Başarılı ";
-                }
-                catch (Exception)
-                {
-                    _Result.Status = false;
-                    _Result.Message = "Hata Oluştu.";
-                }
+            try
+            {
+                if (sql != "") db.Database.ExecuteSqlCommand(sql);
+                _Result.Status = true;
+                _Result.Message = "İşlem Başarılı ";
+            }
+            catch (Exception ex)
+            {
+                _Result.Status = false;
+                _Result.Message = "Hata Oluştu.";
             }
 
             return Json(_Result, JsonRequestBehavior.AllowGet);
