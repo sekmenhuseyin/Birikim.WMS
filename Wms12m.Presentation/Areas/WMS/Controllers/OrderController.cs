f774fe3bbb6c1101e48520e95c8cba9d1d23b2b4
BT <bty@12mconsulting.com.tr>
2018-01-15 09:52:04 +0300

    web sipariş sayfa eklendi

---
+++ Wms12m.Presentation/Areas/WMS/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Wms12m.Presentation.Areas.WMS.Controllers
+{
+    public class OrderController : RootController
+    {
+        // GET: WMS/Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
