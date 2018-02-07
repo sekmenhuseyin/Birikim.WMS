7f439370c9742a4f0993ce9d623b7803c6296849
BT <bty@12mconsulting.com.tr>
2018-02-07 12:16:49 +0300

    stok rapor

--- Wms12m.Entity/VM_WMS/Charts.cs
+++ Wms12m.Entity/VM_WMS/Charts.cs
@@ -422,6 +422,23 @@ namespace Wms12m.Entity
         public decimal? AlacakBakiye { get; set; }
     }
 
+    public class SevkiyatKalanRapor
+    {
+        /// <summary> VarChar(30) (Not Null) </summary>
+        public string SeriNo { get; set; }
+        /// <summary> Char(10) (Allow Null) </summary>
+        public string Tarih { get; set; }
+        /// <summary> VarChar(30) (Not Null) </summary>
+        public string MalKodu { get; set; }
+        /// <summary> VarChar(10) (Not Null) </summary>
+        public string Depo { get; set; }
+        /// <summary> Decimal(38,6) (Allow Null) </summary>
+        public decimal? Miktar { get; set; }
+        /// <summary> Decimal(38,6) (Allow Null) </summary>
+        public decimal? KalanStok { get; set; }
+
+    }
+
 
     public class RP_TahsilatKontrol
     {
