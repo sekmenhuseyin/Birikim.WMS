70cfbf5ecd88252694b70c2ffd0ba79cc63c628d
BT <bty@12mconsulting.com.tr>
2018-02-13 09:09:35 +0300

    using

--- Wms12m.Presentation/Areas/Reports/Controllers/FinancialController.cs
+++ Wms12m.Presentation/Areas/Reports/Controllers/FinancialController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
+using Wms12m.Entity;
 
 namespace Wms12m.Presentation.Areas.Reports.Controllers
 {
