cb3ad5907f06d22a3b3845b7593b50890497a306
HS <h.sekmenoglu@12mconsulting.com.tr>
2018-02-14 14:40:22 +0300

    encrypt test

--- Wms12m.Test/Security.cs
+++ Wms12m.Test/Security.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Shouldly;
+using System;
+using Wms12m.Security;
+using Xunit;
 
 namespace Wms12m.Test
 {
     public class Security
     {
+        [Fact]
+        public void EnsureEndsWith_Test()
+        {
+            //create guid for random test
+            var guid = Guid.NewGuid().ToString();
+            //encrypt string
+            var encrypted = CryptographyExtension.Sifrele(guid);
+            var decrypted = CryptographyExtension.Cozumle(encrypted);
+            //decrypy and test
+            CryptographyExtension.Cozumle(encrypted).ShouldBe(guid);
+            Assert.Equal(guid, decrypted);
+        }
+
     }
 }
