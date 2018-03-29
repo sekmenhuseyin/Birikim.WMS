ca173800450020af753c3cc18c0b5d22d5e93ee6
BT <bty@12mconsulting.com.tr>
2018-03-29 16:36:12 +0300

    çisel hanımın maili üzerine değişiklikler

--- Wms12m.Entity/VM_WMS/Rapor/GunlukSiparis.cs
+++ Wms12m.Entity/VM_WMS/Rapor/GunlukSiparis.cs
@@ -4,6 +4,7 @@
     {
         /// <summary> VarChar(16) (Not Null) </summary>
         public string EVRAKNO { get; set; }
+        public decimal Tutar { get; set; }
         /// <summary> Int (Not Null) </summary>
         public string Tarih { get; set; }
         /// <summary> VarChar(20) (Not Null) </summary>
@@ -16,7 +17,8 @@
         public string GrupKod { get; set; }
 
         public static string Sorgu = @"
-                           SELECT    SPI.EVRAKNO,
+                          SELECT    SPI.EVRAKNO,
+SPI.Tutar as Tutar,
  CONVERT(VARCHAR(10),CONVERT(datetime,SPI.Tarih-2),104)as Tarih,
 SPI.CHK,(CHK.Unvan1+' '+CHK.Unvan2) as Unvan,CHK.TipKod,CHK.GrupKod
           FROM FINSAT6{0}.FINSAT6{0}.SPI AS SPI WITH (NOLOCK)
@@ -24,7 +26,7 @@ SPI.CHK,(CHK.Unvan1+' '+CHK.Unvan2) as Unvan,CHK.TipKod,CHK.GrupKod
           INNER JOIN FINSAT6{0}.FINSAT6{0}.CHK AS CHK WITH (NOLOCK) ON CHK.HesapKodu = SPI.CHK
           WHERE SPI.KynkEvrakTip=62
 		  AND SPI.Tarih BETWEEN {1} AND {2}
-          Group By SPI.EvrakNo,SPI.Tarih, SPI.Chk, (CHK.Unvan1+' '+CHK.Unvan2), CHK.TipKod,CHK.GrupKod
+          Group By SPI.EvrakNo,SPI.Tutar,SPI.Tarih, SPI.Chk, (CHK.Unvan1+' '+CHK.Unvan2), CHK.TipKod,CHK.GrupKod
 
                                    ";
     }
--- Wms12m.Entity/VM_WMS/Rapor/WebSiparis.cs
+++ Wms12m.Entity/VM_WMS/Rapor/WebSiparis.cs
@@ -23,7 +23,8 @@
                                     DECLARE @TAR1 INT,@TAR2 INT
                                     SET @TAR1 = FINSAT6{0}.dbo.AyIlkSonGun({1},{2},1)
                                     SET @TAR2 = FINSAT6{0}.dbo.AyIlkSonGun({1},{2},0)
-                                    SELECT {4}
+									SELECT * FROM (
+                                    SELECT 
                                     CONVERT(NVARCHAR(10),DATEADD(DD,SPI.Tarih,'1899-12-30'),104) AS Tarih,
                                     SPI.EvrakNo,
                                     CHK.TipKod,
@@ -31,13 +32,15 @@
                                     SPI.Chk AS HesapKodu,
                                     CONCAT(CHK.Unvan1,SPACE(1),CHK.Unvan2) AS Unvan,
                                     SUM(SPI.Tutar - SPI.ToplamIskonto) AS Toplam,
-                                    (CASE WHEN SPI.SiparisDurumu = 1 THEN 'Kapalı' ELSE 'Açık' END) AS SiparisDurumu
+									case (select count(*) FROM FINSAT6{0}.FINSAT6{0}.SPI (NOLOCK) SS  WHERE SS.EVRAKNO=SPI.EvrakNo AND SS.Tarih=SPI.Tarih and SPI.KynkEvrakTip=SS.KynkEvrakTip and SS.CHK=SPI.CHK and ss.SiparisDurumu=0) when 0 then 'Kapalı' else 'Açık' end as SiparisDurumu 
                                     FROM FINSAT6{0}.FINSAT6{0}.SPI AS SPI WITH (NOLOCK)
                                     INNER JOIN FINSAT6{0}.FINSAT6{0}.STK AS STK WITH (NOLOCK) ON STK.MALKODU=SPI.MALKODU 
                                     INNER JOIN FINSAT6{0}.FINSAT6{0}.CHK AS CHK WITH (NOLOCK) ON CHK.HesapKodu=SPI.Chk 
                                     WHERE SPI.KynkEvrakTip = 62 AND (SPI.Tarih BETWEEN @TAR1 AND @TAR2) 
                                     {3}
-                                    GROUP BY SPI.Tarih,SPI.EvrakNo,CHK.TipKod,CHK.GrupKod,SPI.Chk,CHK.Unvan1,CHK.Unvan2,SPI.SiparisDurumu
+                                    GROUP BY   SPI.Tarih,SPI.EvrakNo,CHK.TipKod,CHK.GrupKod,SPI.Chk,CHK.Unvan1,CHK.Unvan2,SPI.KynkEvrakTip
+									) AS D
+									ORDER BY D.EVRAKNO
                                     ";
     }
 }
