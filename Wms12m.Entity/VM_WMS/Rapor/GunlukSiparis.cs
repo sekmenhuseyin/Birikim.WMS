8674407cad1c9f33006c15cd3f46b68577d93bff
BT <bty@12mconsulting.com.tr>
2018-04-02 17:41:03 +0300

    sum(SPI.Tutar)-sum(SPI.IskontoTutar1)

--- Wms12m.Entity/VM_WMS/Rapor/GunlukSiparis.cs
+++ Wms12m.Entity/VM_WMS/Rapor/GunlukSiparis.cs
@@ -17,15 +17,16 @@
         public string GrupKod { get; set; }
 
         public static string Sorgu = @"
-                          SELECT    SPI.EVRAKNO,
-sum(SPI.Tutar) as Tutar,
- CONVERT(VARCHAR(10),CONVERT(datetime,SPI.Tarih-2),104)as Tarih,
+
+SELECT    SPI.EVRAKNO,
+sum(SPI.Tutar)-sum(SPI.IskontoTutar1)as Tutar,
+CONVERT(VARCHAR(10),CONVERT(datetime,SPI.Tarih-2),104)as Tarih,
 SPI.CHK,(CHK.Unvan1+' '+CHK.Unvan2) as Unvan,CHK.TipKod,CHK.GrupKod
           FROM FINSAT6{0}.FINSAT6{0}.SPI AS SPI WITH (NOLOCK)
           INNER JOIN FINSAT6{0}.FINSAT6{0}.STK AS STK WITH (NOLOCK) ON STK.MALKODU = SPI.MALKODU 
           INNER JOIN FINSAT6{0}.FINSAT6{0}.CHK AS CHK WITH (NOLOCK) ON CHK.HesapKodu = SPI.CHK
           WHERE SPI.KynkEvrakTip=62
-		  AND SPI.Tarih BETWEEN {1} AND {2}
+          AND SPI.Tarih BETWEEN {1} AND {2}
           Group By SPI.EvrakNo,SPI.Tarih, SPI.Chk, (CHK.Unvan1+' '+CHK.Unvan2), CHK.TipKod,CHK.GrupKod
 
                                    ";
