266c4c2e555215a9d20c1f2e1bf846daf5441d36
BT <bty@12mconsulting.com.tr>
2018-04-10 10:43:04 +0300

    clas duzeltme

--- Wms12m.Entity/VM_YeniNesil/frmOnaySiparisList.cs
+++ Wms12m.Entity/VM_YeniNesil/frmOnaySiparisList.cs
@@ -21,9 +21,32 @@
         public string Tarih { get; set; }
         /// <summary> VarChar(800) (Not Null) </summary>
         public string Notlar { get; set; }
+        /// <summary> NVarChar(30) (Allow Null) </summary>
+        public string MalKodu { get; set; }
+        /// <summary> NVarChar(50) (Allow Null) </summary>
+        public string MalAdi { get; set; }
+        /// <summary> NVarChar(10) (Allow Null) </summary>
+        public string Depo { get; set; }
+        /// <summary> Decimal(23,6) (Allow Null) </summary>
+        public decimal? BirimFiyat { get; set; }
+        /// <summary> Decimal(19,2) (Allow Null) </summary>
+        public decimal? Tutar { get; set; }
+        /// <summary> NVarChar(3) (Allow Null) </summary>
+        public string DovizCinsi { get; set; }
+        /// <summary> Decimal(19,1) (Allow Null) </summary>
+        public decimal? EsikFiyat { get; set; }
         /// <summary> VarChar(15) (Allow Null) </summary>
         public string OnayRedTarih { get; set; }
         /// <summary> NVarChar(20) (Allow Null) </summary>
         public string OnaylayanReddeden { get; set; }
+
+
+
+
+
+
+
+
+
     }
 }
